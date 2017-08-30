910522883783027d3476c75e7319f12bfd4bf1aa
Matthieu Lafaysse <matthieu.lafaysse@meteo.fr>
2017-08-30 17:36:50 +0200

    Can add fields not present in the initial namelist

--- tools/update_namelist.py
+++ tools/update_namelist.py
@@ -14,26 +14,46 @@ import six
 class update_surfex_namelist(object):
     """Class with routines to update SURFEX namelist"""
 
-    def __init__(self,datebegin,forcing="FORCING.nc",updateloc=True):
+    def __init__(self,datebegin,forcing="FORCING.nc",updateloc=True,dateforcbegin=None,dateforcend=None):
         """Call the subroutines for updating the SURFEX namelists. Updateloc is only optional."""
         dic=self.update_dates(datebegin)
         if updateloc:
             dic=self.updates_loc(forcing, dic)
+            
+        if dateforcbegin is not None:
+            self.update_forcingdates(datebegin, dateforcbegin, dateforcend, dic)
+            
         self.modify_namelist(dic)
     
     def update_dates(self,datebegin,dic={}):
         """Dictionnary describing the lines to modify in SURFEX namelist for defining the beginning of the simulation."""
+        """The values of the dictionnary are tuples: first element is the name of the namelist, second element is the field itself."""
+
         
-        dic["NYEAR"]="    NYEAR = " + str(datebegin.year)
-        dic["NMONTH"]="    NMONTH = " +str(datebegin.month)
-        dic["NDAY"]="    NDAY = " +str(datebegin.day)
-        dic["XTIME"]="    XTIME = " +str(datebegin.hour*3600.)
+        dic["NYEAR"] =("NAM_PREP_SURF_ATM","    NYEAR = " + str(datebegin.year))
+        dic["NMONTH"]=("NAM_PREP_SURF_ATM","    NMONTH = " +str(datebegin.month))
+        dic["NDAY"]  =("NAM_PREP_SURF_ATM","    NDAY = " +str(datebegin.day))
+        dic["XTIME"] =("NAM_PREP_SURF_ATM","    XTIME = " +str(datebegin.hour*3600.))
         
         return dic
+
+    def update_forcingdates(self,datebegin,dateforcbegin,dateend,dateforcend,dic={}):
+        """Dictionnary describing the lines to modify in SURFEX namelist for limiting the dates to read in a FORCING file longer than the simulation."""
+        """The values of the dictionnary are tuples: first element is the name of the namelist, second element is the field itself."""
+                
+        checkdateafter(datebegin,dateforcbegin)
+        checkdatebefore(dateend,dateforcend)
+        
+        if datebegin>dateforcbegin:
+            dic["LDELAYEDSTARTNC"]=("NAM_IO_OFFLINE","    LDELAYEDSTARTNC = T")
+        if dateend<dateforcend:
+            dic["NDATESTOP"]=("NAM_IO_OFFLINE","    NDATESTOP = "+dateend.strftime("%Y, %m, %d, ")+str(dateend.hour*3600) )  
+        
         
     def updates_loc(self,forcing="FORCING.nc",dic={}):
         """Dictionnary describing the lines to modify in SURFEX namelist for defining the coordinates of the simulation points."""
-
+        """The values of the dictionnary are tuples: first element is the name of the namelist, second element is the field itself."""
+        
         #Read coordinates in FORCING file
         forc=prosimu(forcing)
         latitudes1d=forc.read("LAT")
@@ -45,25 +65,24 @@ class update_surfex_namelist(object):
         dlon1d=np.zeros_like(longitudes1d)+0.5
     
         # Strings to write in the namelist
-        dic["XY"] = "    XY = "
-        dic["XX"] = "    XX = "
-        dic["XDY"] = "    XDY = "
-        dic["XDX"]= "    XDX = "
+        dic["XY"] = ("NAM_LONLATVAL", "    XY = ")
+        dic["XX"] = ("NAM_LONLATVAL", "    XX = ")
+        dic["XDY"]= ("NAM_LONLATVAL", "    XDY = ")
+        dic["XDX"]= ("NAM_LONLATVAL", "    XDX = ")
     
-        for val in latitudes1d : dic["XY"] = dic["XY"] + str(val) + ","
-        for val in longitudes1d : dic["XX"] = dic["XX"] + str(val) + ","
-        for val in dlat1d : dic["XDY"] = dic["XDY"] + str(val) + ","
-        for val in dlon1d : dic["XDX"] = dic["XDX"] + str(val) + ","
+        for val in latitudes1d : dic["XY"][1] = dic["XY"][1] + str(val) + ","
+        for val in longitudes1d : dic["XX"][1] = dic["XX"][1] + str(val) + ","
+        for val in dlat1d : dic["XDY"][1] = dic["XDY"][1] + str(val) + ","
+        for val in dlon1d : dic["XDX"][1] = dic["XDX"][1] + str(val) + ","
     
         #Number of simulation points
-        dic["NPOINTS"] = "    NPOINTS = " + str(len(longitudes1d))
+        dic["NPOINTS"] = ("NAM_LONLATVAL","    NPOINTS = " + str(len(longitudes1d)))
     
         return dic
         
     def modify_namelist(self,dic):
         """Routine to modify a reference SURFEX namelist according to a dictionnary of fields to modify."""    
 
-        
         if not os.path.isfile("OPTIONS.nam"):
             sys.exit("ERREUR A GERER")
             
@@ -71,20 +90,68 @@ class update_surfex_namelist(object):
         
         # Open reference namelist
         namSURFEX_base=open("OPTIONS_base.nam",'r')
+                
         # Open new namelist
         namSURFEX=open("OPTIONS.nam",'w')
     
         # Loop over the lines of the old namelist. Copy everything but lines including the keys of the dictionnary
         # These lines are replaced by the values of the dictionnary
         for line in namSURFEX_base:
-            newline = line
-            for key,value in six.iteritems(dic): 
-                if key in line:
-                    newline = value + '\n'
-            namSURFEX.write(newline)
-            
+            newline=line
+            if "&NAM" in line:
+                # This is the opening key of the namelist
+                # Get the name of the namelist
+                namelistopen=line.split()[0][1:]
+            else:
+                for key,value in six.iteritems(dic):
+                    (namelist,field)=value[:]
+                    # If the namelist corresponds to the current namelist
+                    if namelist==namelistopen:
+                        if key in line:
+                            # Replace the line by the field 
+                            newline = field + '\n'
+                            # Remove the field to update in the dictionnary
+                            dic.pop(key)
+                        elif "/" in line:
+                            # This is the end of the namelist.
+                            # The field was not in the initial namelist, it must be added, as well as the / line to finish the namelist
+                            newline = field + '\n\\\n'
+                            # Remove the field to update in the dictionnaryb=                            
+                            dic.pop(key)
+                            namelistopen=""
+            # Write the new line in the new namelist
+            namSURFEX.write(newline)    
         # Close both namelists
         namSURFEX_base.close()
         namSURFEX.close()
     
+class DateException(Exception):
     
+    def __init__(self,message=""):
+        self.message=message
+    
+    def __str__(self):
+        return "Dates inconsitency : "+self.message
+    
+class EarlyDateException(DateException):
+    
+    def __init__(self,earlydate,date):
+        self.message=earlydate.strftime()+ "is before"+date.strftime()
+
+class LateDateException(DateException):
+    
+    def __init__(self,latedate,date):
+        self.message=latedate.strftime()+ "is after"+date.strftime()
+        
+def checkdatebefore(date,datemax):
+    if date> datemax:
+        raise LateDateException(date,datemax)
+
+def checkdateafter(date,datemin):
+    if date< datemin:
+        raise EarlyDateException(date,datemin)
+
+def checkdatebetween(date,datemin,datemax):
+    checkdateafter(date, datemin)
+    checkdatebefore(date, datemax)
+
