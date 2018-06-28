53911963507d15b4bee19aae8a9743268c3522f0
Matthieu Lafaysse <matthieu.lafaysse@meteo.fr>
2018-06-28 17:32:40 +0200

    correction

--- tools/special_diags.py
+++ tools/special_diags.py
@@ -1,9 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 '''
 Created on 28 juin 2018
 
 @author: lafaysse
 
 Template for Jesus developments (special SURFEX post-processing)
+Note that in the general case, we should avoid to add new diagnostics here, all diagnostics shoud be implemented in SURFEX when possible
 
 '''
 
@@ -13,18 +17,23 @@ from utils.prosimu import prosimu
 
 class diagprosimu(prosimu):
 
+    # Define variable names
+    varname_dz = "SNOWDZ"
+    varname_snowdepth = "SNOWDEPTH"
+    varname_impcontent = "XXXXXXXXXXXXXXXXXXXX"
+
     def __init__(self, *args, **kwargs):
         # Get dimensions
         super(diagprosimu, self).__init__(self, *args, **kwargs)
-        self.dimensions = self.getdimvar("SNOWDEPTH")
+        self.dimensions = self.getdimvar(self.varname_snowdepth)
 
     def add_surface_impcontent(self):
 
         # Read snow layers thicknesses
-        snowdz = self.read("SNOWDZ")
+        snowdz = self.read(self.varname_dz)
 
         # Read impurities content from SURFEX file
-        impcontent_bylayer = self.read("XXXXXXXXXXXXXXXXXXXX")
+        impcontent_bylayer = self.read(self.varname_impcontent)
 
         # Compute the mean impurity content for the first 5 mm of SWE from the surface
         # Write here the correct algorithm
@@ -38,6 +47,9 @@ class diagprosimu(prosimu):
 
 
 if __name__ == "__main__":
-    mypro = diagprosimu("PRO.nc", openmode="a")
+    import sys
+    profile = sys.argv[1]
+
+    mypro = diagprosimu(profile, openmode="a")
     mypro.add_surface_impcontent()
     mypro.close()
