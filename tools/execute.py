1c5dffcf19e45363902011c87762cb82ebd57daf
Matthieu Lafaysse <matthieu.lafaysse@meteo.fr>
2018-09-25 12:17:29 +0200

    python equivalent of ulimit -s unlimited

--- tools/execute.py
+++ tools/execute.py
@@ -31,8 +31,10 @@ def callSystemOrDie(commande, errorcode=None):
 def callSurfexOrDie(commande, moderun="NORMAL", nproc=1, errorcode=None):
     '''Execute a SURFEX binary'''
 
-    # Without the following line, the worse segmentation faults you can ever imagine
-    os.system("ulimit -s unlimited")
+    # Without the following lines, the worse segmentation faults you can ever imagine
+    # Equivalent to bash command ulimit -s unlimited
+    import resource
+    resource.setrlimit(resource.RLIMIT_STACK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
 
     os.environ["OMP_NUM_THREADS"] = "1"
     if ("PGD" in commande or "PREP" in commande) and moderun in ["MPI", "MPIRUN"]:
