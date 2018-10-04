ec60c24a41779d8024f18de3992173a2cc86eacf
Hugo Merzisen <hugo.merzisen@meteo.fr>
2018-10-04 17:01:35 +0200

    Méthode de lecture et sous-selection sur une variable netcdf ajoutée

--- utils/prosimu.py
+++ utils/prosimu.py
@@ -16,8 +16,20 @@ from utils.FileException import FileNameException, FileOpenException, VarNameExc
 
 
 class prosimu():
-    def __init__(self, path, ncformat='NETCDF3_CLASSIC', openmode='r'):
 
+
+    # Variables permettant de gérer un nom de dimension différent dans un
+    # fichier netcdf (par exemple si on veut lire un fichier dans l'ancien
+    # format de la chaîne ou Number_of_points s'appelle location:
+    # > pro = prosimu(monnetcdf)
+    # > pro.Number_of_points = 'location'
+    # et voilà
+    # (pris en compte dans la méthode read_var seulement)
+    Number_of_points = 'Number_of_points'
+    Number_of_Patches = 'Number_of_Patches'
+
+
+    def __init__(self, path, ncformat='NETCDF3_CLASSIC', openmode='r'):
         if type(path) is list:
             for fichier in path:
                 if not os.path.isfile(fichier):
@@ -49,6 +61,8 @@ class prosimu():
             print(path)
             print(type(path))
             raise FileNameException(path)
+
+
 
     def format(self):
         try:
@@ -107,11 +121,113 @@ class prosimu():
             time_base = self.dataset.variables["time"]
             time_base.calendar = 'standard'
             time = netCDF4.MFTime(time_base)
-#             time=time_base
+            # time=time_base
         else:
             time = self.dataset.variables["time"]
 
         return np.array(netCDF4.num2date(time[:], time.units))
+
+    def get_range(self,dimname,conditions):
+        """
+        Pour plus de lisibilité
+        """
+        if dimname not in conditions:
+            # si dimension n'est pas dans les arguments d'appel, on met le range
+            # complet de la dimension
+            ncdim=self.dataset.dimensions[dimname]
+            return np.arange(ncdim.size)
+        if dimname in self.listvar():
+            # s'il existe une variable contenant des valeurs pour la dimension,
+            # le filtrage se fait sur les valeurs
+            dimvar = self.dataset.variables[dimname]
+            if hasattr(conditions[dimname],'__iter__'):
+                # filtrage sur un ensemble de valeurs (tout itérable)
+                return np.where(
+                    np.isin(dimvar[:],conditions[dimname]))[0]
+            else:
+                # cas d'une valeur unique
+                return np.where(dimvar[:] == conditions[dimname])[0][0]
+        # dans tous les autres cas
+        return conditions[dimname]
+
+
+    def read_var(self,variable_name,**kwargs):
+        """
+        variable_name : nom de la variable
+        **kwargs : spécifier la sous-sélection sous la forme variable=iterable
+        ou variable est le nom d'une dimension, ou d'une variable du netcdf
+
+        et iterable est un itérable contenant les valeurs auxquelles on veut restreindre
+        la lecture
+        Retourne : un tableau numpy
+
+        Exemples:
+            snowtemp = prosimu.read_var('SNOWTEMP',time=0,Number_of_points = np.arange(100,125))
+            snowtemp2 = prosimu.read_var('SNOWTEMP',ZS=3600,slope = [0,20])
+        """
+        # Gestion des noms de dimensions différents entre ancien et nouveau
+        # format
+        condition_points = kwargs.get('Number_of_points')
+        condition_patches = kwargs.get('Number_of_patches')
+        if condition_points:
+            del kwargs['Number_of_points']
+            kwargs[self.Number_of_points] = condition_points
+        if condition_patches:
+            del kwargs['Number_of_Patches']
+            kwargs[self.Number_of_Patches] = condition_patches
+
+        # valeurs par défaut
+        if self.Number_of_Patches not in kwargs.keys():
+            kwargs[self.Number_of_Patches] = 0
+        # contrôles des arguments d'appel de la méthode
+        if variable_name not in self.listvar():
+            raise VarNameException(variable_name, self.path)
+        # cas particulier des variables permettant un filtrage géographique
+        geo_keys = [key for key in kwargs.keys()
+                    if ((key in self.dataset.variables.keys())
+                    and (self.dataset.variables[key].dimensions == (self.Number_of_points,)))]
+        points_restriction = self.get_points(**{key:kwargs[key] for key in geo_keys})
+        if self.Number_of_points in kwargs:
+            nop = kwargs[self.Number_of_points]
+            if not(hasattr(nop,'__iter__')):
+                nop = [nop]
+            kwargs[self.Number_of_points] = list(set(points_restriction).intersection(nop))
+        else:
+            kwargs[self.Number_of_points] = points_restriction
+        # le filtrage
+        localisation_variables = \
+            [varname
+             for varname,var in self.dataset.variables.iteritems()
+             if var.dimensions == (self.Number_of_points,)]
+        for name,value in kwargs.iteritems():
+            if not(name in  set(self.dataset.dimensions.keys()).union(localisation_variables)):
+                raise TypeError(
+                    "%s n'est ni une dimension ni une variable du dataset autorisant le filtrage"%(name,))
+        slices = []
+        ncvariable = self.dataset.variables[variable_name]
+        dims = ncvariable.dimensions
+        for dimname in dims:
+            slices.append(self.get_range(dimname,kwargs))
+        slices = tuple(slices)
+        return ncvariable[slices]
+
+
+    def get_points(self,**kwargs):
+        """
+        Renvoie les valeurs de la dimension Number_of_points correspondant à une
+        sous-selection de variables aspect,ZS,massif_num,slope
+        """
+        if not( all(
+            [(self.dataset.variables[varname].dimensions == (self.Number_of_points,))
+            for varname in kwargs.keys()]
+        )):
+            raise TypeError("""Le filtrage ne peut se faire que sur des variables géographiques (ZS, slope, aspect, massif_num)""")
+        nop = np.arange(len(self.dataset.dimensions[self.Number_of_points]))
+        locations_bool = np.ones(len(nop))
+        for varname,values in kwargs.iteritems():
+            locations_bool = np.logical_and(locations_bool,np.isin(self.dataset.variables[varname],values))
+        return np.where(locations_bool)[0]
+
 
     def extract(self, varname, var, selectpoint=-1, removetile=True, hasTime = True):
 
@@ -279,7 +395,9 @@ class prosimu():
         else:
             return array
 
+
     # Pour compatibilité anciens codes, on conserve les routines obsolètes read1d et read2d
+
     def read1d(self, varname, fill2zero=False, indpoint=0):
         return self.read(varname, fill2zero=fill2zero, selectpoint=indpoint)
 
