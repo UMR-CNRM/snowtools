ef9c20588481a79e450c8970b53aeaf4b60658bc
Matthieu Vernay <matthieu.vernay@meteo.fr>
2019-02-06 08:38:40 +0100

    Alternate managment
    Safran alternate guess must be generatedby the prepsaf task.

--- tasks/oper/prepsafran_analyse.py
+++ tasks/oper/prepsafran_analyse.py
@@ -7,7 +7,6 @@ __all__ = []
 import footprints
 logger = footprints.loggers.getLogger(__name__)
 
-import vortex
 from vortex import toolbox
 from vortex.layout.nodes import Driver, Task
 from cen.layout.nodes import S2MTaskMixIn
@@ -65,6 +64,29 @@ class PrepSafran(Task, S2MTaskMixIn):
 
                 self.sh.title('Toolbox input tb01_b')
                 tbarp.extend(toolbox.input(
+                    alternate      = 'Gridpoint',
+                    format         = 'grib',
+                    geometry       = self.conf.arpege_geometry,
+                    kind           = 'gridpoint',
+                    suite          = 'oper',
+                    cutoff         = 'production',
+                    local          = 'mb035/ARPEGE[date::addterm_ymdh]',
+                    date           = '{0:s}/-PT12H'.format(self.conf.rundate.ymd6h),
+                    # Utilisation d'une varibale de conf pour assurer la cohérence des cumuls de precip
+                    term           = self.conf.cumul,
+                    namespace      = 'oper.multi.fr',
+                    nativefmt      = '[format]',
+                    origin         = 'historic',
+                    model          = '[vapp]',
+                    vapp           = self.conf.source_app,
+                    vconf          = self.conf.deterministic_conf,
+                    fatal          = True,
+                ))
+                print t.prompt, 'tb01_b =', tbarp
+                print
+
+                self.sh.title('Toolbox input tb02_a')
+                tbarp.extend(toolbox.input(
                     role           = 'Gridpoint',
                     format         = 'grib',
                     geometry       = self.conf.arpege_geometry,
@@ -82,10 +104,33 @@ class PrepSafran(Task, S2MTaskMixIn):
                     vconf          = self.conf.deterministic_conf,
                     fatal          = True,
                 ))
-                print t.prompt, 'tb01_b =', tbarp
+                print t.prompt, 'tb02_a =', tbarp
                 print
 
-                self.sh.title('Toolbox input tb01_c')
+                self.sh.title('Toolbox input tb02_b')
+                tbarp.extend(toolbox.input(
+                    alternate      = 'Gridpoint',
+                    format         = 'grib',
+                    geometry       = self.conf.arpege_geometry,
+                    kind           = 'gridpoint',
+                    suite          = 'oper',
+                    cutoff         = 'production',
+                    local          = 'mb036/ARPEGE[date::addterm_ymdh]',
+                    date           = '{0:s}/-PT18H'.format(self.conf.rundate.ymd6h),
+                    # Utilisation d'une varibale de conf pour assurer la cohérence des cumuls de precip
+                    term           = self.conf.cumul,
+                    namespace      = 'oper.multi.fr',
+                    nativefmt      = '[format]',
+                    origin         = 'historic',
+                    model          = '[vapp]',
+                    vapp           = self.conf.source_app,
+                    vconf          = self.conf.deterministic_conf,
+                    fatal          = True,
+                ))
+                print t.prompt, 'tb02_b =', tbarp
+                print
+
+                self.sh.title('Toolbox input tb03_a')
                 tbarp.extend(toolbox.input(
                     role           = 'Gridpoint',
                     format         = 'grib',
@@ -104,14 +149,37 @@ class PrepSafran(Task, S2MTaskMixIn):
                     vconf          = self.conf.deterministic_conf,
                     fatal          = True,
                 ))
-                print t.prompt, 'tb01_c =', tbarp
+                print t.prompt, 'tb03_a =', tbarp
+                print
+
+                self.sh.title('Toolbox input tb03_b')
+                tbarp.extend(toolbox.input(
+                    alternate      = 'Gridpoint',
+                    format         = 'grib',
+                    geometry       = self.conf.arpege_geometry,
+                    kind           = 'gridpoint',
+                    suite          = 'oper',
+                    cutoff         = 'production',
+                    local          = 'mb037/ARPEGE[date::addterm_ymdh]',
+                    date           = '{0:s}/-PT24H'.format(self.conf.rundate.ymd6h),
+                    # Utilisation d'une varibale de conf pour assurer la cohérence des cumuls de precip
+                    term           = self.conf.cumul,
+                    namespace      = 'oper.multi.fr',
+                    nativefmt      = '[format]',
+                    origin         = 'historic',
+                    model          = '[vapp]',
+                    vapp           = self.conf.source_app,
+                    vconf          = self.conf.deterministic_conf,
+                    fatal          = True,
+                ))
+                print t.prompt, 'tb03_b =', tbarp
                 print
 
                 # II- Guess PEARP (membres 0 à 34)
                 # --------------------------------
 
                 # Récupération du réseau 6h (J-1) (utilisée seulement à partir du run de 6h) pour l'analyse (J-1) 6h -> J 6h
-                self.sh.title('Toolbox input tb02')
+                self.sh.title('Toolbox input tb04_a')
                 tbpearp = toolbox.input(
                     role           = 'Gridpoint',
                     block          = 'forecast',
@@ -131,7 +199,31 @@ class PrepSafran(Task, S2MTaskMixIn):
                     vapp           = self.conf.source_app,
                     vconf          = self.conf.eps_conf,
                 )
-                print t.prompt, 'tb02 =', tbpearp
+                print t.prompt, 'tb04_a =', tbpearp
+                print
+
+                # Alternate : récupération des prévi P12 à P36 du réseau 18h (J-2) pour l'analyse (J-1) 6h -> J 6h
+                self.sh.title('Toolbox input tb04_b')
+                tbpearp = toolbox.input(
+                    alternate      = 'Gridpoint',
+                    block          = 'forecast',
+                    suite          = 'oper',
+                    cutoff         = 'production',
+                    format         = 'grib',
+                    geometry       = self.conf.pearp_geometry,
+                    kind           = 'gridpoint',
+                    local          = 'mb[member]/PEARP[date::addterm_ymdh]',
+                    date           = '{0:s}/-PT36H'.format(self.conf.rundate.ymd6h),
+                    term           = footprints.util.rangex(self.conf.ana_terms, shift=12),
+                    member         = footprints.util.rangex(self.conf.pearp_members),
+                    namespace      = 'vortex.multi.fr',
+                    nativefmt      = '[format]',
+                    origin         = 'historic',
+                    model          = '[vapp]',
+                    vapp           = self.conf.source_app,
+                    vconf          = self.conf.eps_conf,
+                )
+                print t.prompt, 'tb04_b =', tbpearp
                 print
 
             else:
@@ -184,7 +276,8 @@ class PrepSafran(Task, S2MTaskMixIn):
                 vconf          = self.conf.vconf,
                 engine         = 'exec',
                 kind           = 'guess',
-                interpreter    = script[0].resource.language,
+                # interpreter    = script[0].resource.language,
+                interpreter    = 'current',
                 ntasks         = self.conf.ntasks,
                 members        = footprints.util.rangex(self.conf.members),
                 terms          = footprints.util.rangex(self.conf.ana_terms),
@@ -204,7 +297,7 @@ class PrepSafran(Task, S2MTaskMixIn):
             if self.conf.rundate.hour == 3:
 
                 self.sh.title('Toolbox output tb05_a')
-                tb05 = toolbox.output(
+                tb05_a = toolbox.output(
                     role           = 'Ebauche',
                     local          = 'mb035/P[date:yymdh]_[cumul:hour]_[vconf]_assimilation',
                     geometry       = self.conf.domains,
@@ -220,11 +313,31 @@ class PrepSafran(Task, S2MTaskMixIn):
                     source_conf    = self.conf.deterministic_conf,
                     namespace      = self.conf.namespace,
                 ),
-                print t.prompt, 'tb05_a =', tb05
+                print t.prompt, 'tb05_a =', tb05_a
                 print
 
                 self.sh.title('Toolbox output tb05_b')
-                tb05 = toolbox.output(
+                tb05_b = toolbox.output(
+                    alternate      = 'Ebauche',
+                    local          = 'mb035/P[date:yymdh]_[cumul:hour]_[vconf]_production',
+                    geometry       = self.conf.domains,
+                    vconf          = '[geometry:area]',
+                    experiment     = self.conf.xpid,
+                    block          = self.conf.block,
+                    date           = '{0:s}/-PT12H'.format(self.conf.rundate.ymd6h),
+                    cumul          = self.conf.cumul,
+                    nativefmt      = 'ascii',
+                    kind           = 'guess',
+                    model          = 'safran',
+                    source_app     = self.conf.source_app,
+                    source_conf    = self.conf.deterministic_conf,
+                    namespace      = self.conf.namespace,
+                ),
+                print t.prompt, 'tb05_b =', tb05_b
+                print
+
+                self.sh.title('Toolbox output tb06_a')
+                tb06_a = toolbox.output(
                     role           = 'Ebauche',
                     local          = 'mb036/P[date:yymdh]_[cumul:hour]_[vconf]_assimilation',
                     geometry       = self.conf.domains,
@@ -240,11 +353,31 @@ class PrepSafran(Task, S2MTaskMixIn):
                     source_conf    = self.conf.deterministic_conf,
                     namespace      = self.conf.namespace,
                 ),
-                print t.prompt, 'tb05_b =', tb05
+                print t.prompt, 'tb06_a =', tb06_a
                 print
 
-                self.sh.title('Toolbox output tb05_c')
-                tb05 = toolbox.output(
+                self.sh.title('Toolbox output tb06_b')
+                tb06_b = toolbox.output(
+                    alternate      = 'Ebauche',
+                    local          = 'mb036/P[date:yymdh]_[cumul:hour]_[vconf]production',
+                    geometry       = self.conf.domains,
+                    vconf          = '[geometry:area]',
+                    experiment     = self.conf.xpid,
+                    block          = self.conf.block,
+                    date           = '{0:s}/-PT18H'.format(self.conf.rundate.ymd6h),
+                    cumul          = self.conf.cumul,
+                    nativefmt      = 'ascii',
+                    kind           = 'guess',
+                    model          = 'safran',
+                    source_app     = self.conf.source_app,
+                    source_conf    = self.conf.deterministic_conf,
+                    namespace      = self.conf.namespace,
+                ),
+                print t.prompt, 'tb06_b =', tb06_b
+                print
+
+                self.sh.title('Toolbox output tb07_a')
+                tb07_a = toolbox.output(
                     role           = 'Ebauche',
                     local          = 'mb037/P[date:yymdh]_[cumul:hour]_[vconf]_assimilation',
                     geometry       = self.conf.domains,
@@ -260,65 +393,31 @@ class PrepSafran(Task, S2MTaskMixIn):
                     source_conf    = self.conf.deterministic_conf,
                     namespace      = self.conf.namespace,
                 ),
-                print t.prompt, 'tb05_c =', tb05
+                print t.prompt, 'tb07_a =', tb07_a
                 print
 
-                ###############################################################################
-                # A SUPPRIMER DANS LA TACHE OPER
-                ###############################################################################
-#                 self.sh.title('Toolbox output tb06_a')
-#                 tb05 = toolbox.output(
-#                     role           = 'Ebauche',
-#                     local          = 'mb035/P[date:yymdh]_[cumul:hour]_[vconf]_assimilation',
-#                     namespace      = 's2m.archive.fr',
-#                     geometry       = self.conf.domains,
-#                     #vconf          = '[geometry:area]',
-#                     vconf          = self.conf.vconf,
-#                     date           = '{0:s}/-PT12H'.format(self.conf.rundate.ymd6h),
-#                     cumul          = self.conf.cumul,
-#                     nativefmt      = 'ascii',
-#                     kind           = 'guess',
-#                     model          = 'safran',
-#                 ),
-#                 print t.prompt, 'tb05_a =', tb05
-#                 print
-#
-#                 self.sh.title('Toolbox output tb06_b')
-#                 tb05 = toolbox.output(
-#                     role           = 'Ebauche',
-#                     local          = 'mb036/P[date:yymdh]_[cumul:hour]_[vconf]_assimilation',
-#                     namespace      = 's2m.archive.fr',
-#                     geometry       = self.conf.domains,
-#                     vconf          = '[geometry:area]',
-#                     date           = '{0:s}/-PT18H'.format(self.conf.rundate.ymd6h),
-#                     cumul          = self.conf.cumul,
-#                     nativefmt      = 'ascii',
-#                     kind           = 'guess',
-#                     model          = 'safran',
-#                 ),
-#                 print t.prompt, 'tb05_b =', tb05
-#                 print
-#
-#                 self.sh.title('Toolbox output tb06_c')
-#                 tb05 = toolbox.output(
-#                     role           = 'Ebauche',
-#                     local          = 'mb037/P[date:yymdh]_[cumul:hour]_[vconf]_assimilation',
-#                     namespace      = 's2m.archive.fr',
-#                     geometry       = self.conf.domains,
-#                     vconf          = '[geometry:area]',
-#                     date           = '{0:s}/-PT24H'.format(self.conf.rundate.ymd6h),
-#                     cumul          = self.conf.cumul,
-#                     nativefmt      = 'ascii',
-#                     kind           = 'guess',
-#                     model          = 'safran',
-#                 ),
-#                 print t.prompt, 'tb05_c =', tb05
-#                 print
+                self.sh.title('Toolbox output tb07_b')
+                tb07_b = toolbox.output(
+                    alternate      = 'Ebauche',
+                    local          = 'mb037/P[date:yymdh]_[cumul:hour]_[vconf]production',
+                    geometry       = self.conf.domains,
+                    vconf          = '[geometry:area]',
+                    experiment     = self.conf.xpid,
+                    block          = self.conf.block,
+                    date           = '{0:s}/-PT24H'.format(self.conf.rundate.ymd6h),
+                    cumul          = self.conf.cumul,
+                    nativefmt      = 'ascii',
+                    kind           = 'guess',
+                    model          = 'safran',
+                    source_app     = self.conf.source_app,
+                    source_conf    = self.conf.deterministic_conf,
+                    namespace      = self.conf.namespace,
+                ),
+                print t.prompt, 'tb07_b =', tb07_b
+                print
 
-                ##################################################################################
-
-                self.sh.title('Toolbox output tb07')
-                tb06 = toolbox.output(
+                self.sh.title('Toolbox output tb08_a')
+                tb08_a = toolbox.output(
                     role           = 'Ebauche',
                     local          = 'mb[member]/P[date:yymdh]_[cumul:hour]_[vconf]_production',
                     geometry       = self.conf.domains,
@@ -327,7 +426,7 @@ class PrepSafran(Task, S2MTaskMixIn):
                     block          = self.conf.block,
                     cutoff         = 'production',
                     date           = '{0:s}/-PT24H'.format(self.conf.rundate.ymd6h),
-                    cumul          = footprints.util.rangex(footprints.util.rangex(self.conf.ana_terms)),
+                    cumul          = footprints.util.rangex(self.conf.ana_terms),
                     nativefmt      = 'ascii',
                     kind           = 'guess',
                     model          = 'safran',
@@ -336,7 +435,29 @@ class PrepSafran(Task, S2MTaskMixIn):
                     namespace      = self.conf.namespace,
                     member         = footprints.util.rangex(self.conf.pearp_members),
                 ),
-                print t.prompt, 'tb06 =', tb06
+                print t.prompt, 'tb08_a =', tb08_a
+                print
+
+                self.sh.title('Toolbox output tb08_b')
+                tb08_b = toolbox.output(
+                    alternate      = 'Ebauche',
+                    local          = 'mb[member]/P[date:yymdh]_[cumul:hour]_[vconf]_production',
+                    geometry       = self.conf.domains,
+                    vconf          = '[geometry:area]',
+                    experiment     = self.conf.xpid,
+                    block          = self.conf.block,
+                    cutoff         = 'production',
+                    date           = '{0:s}/-PT36H'.format(self.conf.rundate.ymd6h),
+                    cumul          = footprints.util.rangex(self.conf.ana_terms, shift=12),
+                    nativefmt      = 'ascii',
+                    kind           = 'guess',
+                    model          = 'safran',
+                    source_app     = self.conf.source_app,
+                    source_conf    = self.conf.eps_conf,
+                    namespace      = self.conf.namespace,
+                    member         = footprints.util.rangex(self.conf.pearp_members),
+                ),
+                print t.prompt, 'tb08_b =', tb08_b
                 print
 
             else:
@@ -360,27 +481,6 @@ class PrepSafran(Task, S2MTaskMixIn):
                 ),
                 print t.prompt, 'tb05 =', tb05
                 print
-
-#                 ###############################################################################
-#                 # A SUPPRIMER DANS LA TACHE OPER
-#                 ###############################################################################
-#                 self.sh.title('Toolbox output tb06')
-#                 tb05 = toolbox.output(
-#                     role           = 'Ebauche',
-#                     local          = 'mb035/P[date:yymdh]_[cumul:hour]_[vconf]_assimilation',
-#                     namespace      = 's2m.archive.fr',
-#                     geometry       = self.conf.domains,
-#                     vconf          = '[geometry:area]',
-#                     date           = '{0:s}/-PT6H'.format(self.conf.rundate.ymd6h),
-#                     cumul          = self.conf.cumul,
-#                     nativefmt      = 'ascii',
-#                     kind           = 'guess',
-#                     model          = 'safran',
-#                     fatal          = False,
-#                 ),
-#                 print t.prompt, 'tb05 =', tb05
-#                 print
-#                 ################################################################################
 
             from vortex.tools.systems import ExecutionError
             raise ExecutionError('')
