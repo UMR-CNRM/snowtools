1e2689280eb0c5701bbc86d47b4a0efd23e93883
Matthieu Vernay <matthieu.vernay@meteo.fr>
2019-03-22 09:39:10 +0100

    Adaptation to new makeP script
    The new version of the guess generating script is now able to manage
    BDAP-extracted grib files with no interpolation needed, so the default
    intepolation method is None and it is necessary to specify the method
    in other cases.

--- tasks/oper/prepsafran_analyse.py
+++ tasks/oper/prepsafran_analyse.py
@@ -260,7 +260,7 @@ class PrepSafran(Task, S2MTaskMixIn):
                 genv        = 'uenv:s2m.01@vernaym',
                 kind        = 's2m_filtering_grib',
                 language    = 'python',
-                rawopts     = ' -o -a -f ' + ' '.join(list(set([str(rh[1].container.basename) for rh in enumerate(tbarp + tbpearp)]))),
+                rawopts     = ' -o -a -i IDW -f ' + ' '.join(list(set([str(rh[1].container.basename) for rh in enumerate(tbarp + tbpearp)]))),
             )
             print t.prompt, 'tb03 =', tb03
             print
