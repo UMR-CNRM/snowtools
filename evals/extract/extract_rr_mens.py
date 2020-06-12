018bb7edd1496f4b3c3a2a56349268a538f98b8d
Matthieu Lafaysse <matthieu.lafaysse@meteo.fr>
2020-06-12 16:55:05 +0200

    more metadata and extract by massif or by department

--- evals/extract/extract_rr_mens.py
+++ evals/extract/extract_rr_mens.py
@@ -15,8 +15,9 @@ import csv
 from extract_obs import question 
 
 def usage():
-    print("USAGE extract_rr_mens.py datedeb datefin massif")
+    print("USAGE extract_rr_mens.py datedeb datefin numero bydep")
     print("format des dates : YYYYMMDD")
+    print("bydep=0 means numero is massif number ; bydep=1 means numero is department number")
     sys.exit(1)
 
 
@@ -27,7 +28,11 @@ if __name__ == "__main__":
         usage()
     datedeb = sys.argv[1] + "00"
     datefin = sys.argv[2] + "23"
-    massif_nivo = sys.argv[3]
+    bydep = int(sys.argv[4])
+    if (bydep == 0):
+        massif_nivo = sys.argv[3]
+    if (bydep == 1):
+        department = sys.argv[3]
 
     # II.1.1 construction de la question pour les postes nivo pour la HTN
     # ---------------------------------------------------------------------------
@@ -35,9 +40,16 @@ if __name__ == "__main__":
 
     question1 = question("question_jesus.q")
     question1.set_fileout("RRmassif" + str(massif_nivo) + ".obs")
-    question1.set_varout(["dat", "mensq.num_poste", "poste_nivo.nom_usuel", "rr", "rr_me", "nbrr"])
-    question1.set_tables(["MENSQ", "POSTE_NIVO"])
-    question1.set_joincondition("MENSQ.NUM_POSTE = POSTE_NIVO.NUM_POSTE and massif_nivo = " + str(massif_nivo))
+    if (bydep == 0):
+        question1.set_varout(["dat", "mensq.num_poste", "poste_nivo.nom_usuel", "poste_nivo.alti", "rr", "nbrr", "rr_me"])
+        question1.set_tables(["MENSQ", "POSTE_NIVO"])
+        question1.set_joincondition("MENSQ.NUM_POSTE = POSTE_NIVO.NUM_POSTE and massif_nivo = " + str(massif_nivo))
+
+    if (bydep == 1):
+        question1.set_varout(["dat", "mensq.num_poste", "poste_nivo.nom_usuel", "poste_nivo.alti", "poste_nivo.massif_nivo", "rr", "nbrr", "rr_me"])
+        question1.set_tables(["MENSQ", "POSTE_NIVO"])
+        question1.set_joincondition("MENSQ.NUM_POSTE = POSTE_NIVO.NUM_POSTE and num_dep = " + str(department))
+
     question1.set_period_monthly(datedeb, datefin)
     question1.set_order(["dat", "num_poste"])
     question1.run()
