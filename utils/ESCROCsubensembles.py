8dd5d851d257704c66e171ff19b8098470407471
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2020-10-08 09:21:49 +0200

    Add ESCROC ensembles E2MIP and E2MIPtartes

--- utils/ESCROCsubensembles.py
+++ utils/ESCROCsubensembles.py
@@ -32,8 +32,14 @@ class ESCROC_subensembles(dict):
         elif subensemble in ["E2", "E2open"]:
             self.physical_options, self.snow_parameters, self.members = self.E2(members)
 
-        elif subensemble in ["E2Tartes", "E2tartes"]: # Tuzet et al TC 2020
+        elif subensemble == "E2MIP":
+            self.physical_options, self.snow_parameters, self.members = self.E2MIP(members)
+
+        elif subensemble == "E2tartes": # Tuzet et al TC 2020 (Lautaret)
             self.physical_options, self.snow_parameters, self.members = self.E2tartes(members)
+
+        elif subensemble == "E2MIPtartes": # Dumont, Tuzet et al JGR 2020 (Russia)
+            self.physical_options, self.snow_parameters, self.members = self.E2MIPtartes(members)
 
         elif subensemble in ["Crocus"]:
             self.physical_options, self.snow_parameters, self.members = self.Crocus(members)
@@ -148,6 +154,55 @@ class ESCROC_subensembles(dict):
             snow_parameters.append(sp)
 
         return physical_options, snow_parameters, members
+    
+    def E2MIP(self, members):
+
+        allmembers = {1: ['V12', 'C13', 'B60', 'RI2', 'Y81', 'SPK', 'T11', 'CV30000'],
+                      2: ['V12', 'C13', 'TAR', 'RI1', 'Y81', 'B02', 'S14', 'CV30000'],
+                      3: ['V12', 'C13', 'TA+', 'M98', 'Y81', 'SPK', 'S14', 'CV10000'],
+                      4: ['V12', 'F06', 'B60', 'RI2', 'Y81', 'SPK', 'T11', 'CV10000'],
+                      5: ['V12', 'F06', 'B10', 'RI2', 'I02', 'SPK', 'B92', 'CV50000'],
+                      6: ['V12', 'F06', 'B10', 'M98', 'Y81', 'SPK', 'B92', 'CV10000'],
+                      7: ['V12', 'S-F', 'B60', 'RIL', 'Y81', 'SPK', 'T11', 'CV10000'],
+                      8: ['V12', 'S-F', 'B60', 'RI1', 'Y81', 'SPK', 'T11', 'CV50000'],
+                      9: ['V12', 'S-F', 'B60', 'RI1', 'I02', 'B92', 'T11', 'CV50000'],
+                      10: ['V12', 'S-F', 'TA+', 'M98', 'I02', 'B02', 'B92', 'CV10000'],
+                      11: ['S02', 'C13', 'B60', 'RIL', 'Y81', 'B92', 'S14', 'CV50000'],
+                      12: ['S02', 'C13', 'B60', 'M98', 'Y81', 'SPK', 'S14', 'CV30000'],
+                      13: ['S02', 'C13', 'B10', 'RIL', 'I02', 'B92', 'S14', 'CV50000'],
+                      14: ['S02', 'C13', 'B10', 'M98', 'I02', 'SPK', 'T11', 'CV10000'],
+                      15: ['S02', 'C13', 'TA+', 'RI2', 'Y81', 'B02', 'B92', 'CV50000'],
+                      16: ['S02', 'F06', 'B60', 'RIL', 'I02', 'SPK', 'B92', 'CV30000'],
+                      17: ['S02', 'F06', 'B60', 'RI2', 'Y81', 'B92', 'T11', 'CV10000'],
+                      18: ['S02', 'F06', 'B10', 'RI1', 'I02', 'B92', 'S14', 'CV30000'],
+                      19: ['S02', 'F06', 'TAR', 'RI2', 'I02', 'B02', 'B92', 'CV50000'],
+                      20: ['S02', 'F06', 'TA+', 'RIL', 'I02', 'SPK', 'T11', 'CV10000'],
+                      21: ['S02', 'F06', 'TA+', 'M98', 'I02', 'B92', 'B92', 'CV50000'],
+                      22: ['S02', 'S-F', 'B10', 'RIL', 'I02', 'SPK', 'T11', 'CV50000'],
+                      23: ['S02', 'S-F', 'TAR', 'RI2', 'I02', 'B92', 'B92', 'CV30000'],
+                      24: ['A76', 'C13', 'B60', 'RI1', 'Y81', 'SPK', 'T11', 'CV30000'],
+                      25: ['A76', 'C13', 'B60', 'RI2', 'I02', 'B92', 'S14', 'CV50000'],
+                      26: ['A76', 'C13', 'B10', 'RIL', 'I02', 'B02', 'B92', 'CV10000'],
+                      27: ['A76', 'C13', 'B10', 'M98', 'I02', 'SPK', 'T11', 'CV50000'],
+                      28: ['A76', 'C13', 'TAR', 'RI1', 'Y81', 'B92', 'B92', 'CV50000'],
+                      29: ['A76', 'C13', 'TA+', 'RIL', 'I02', 'B02', 'S14', 'CV50000'],
+                      30: ['A76', 'F06', 'B10', 'M98', 'I02', 'B92', 'T11', 'CV30000'],
+                      31: ['A76', 'F06', 'TA+', 'RI1', 'Y81', 'B92', 'T11', 'CV10000'],
+                      32: ['A76', 'F06', 'TA+', 'RI2', 'I02', 'B92', 'T11', 'CV10000'],
+                      33: ['A76', 'S-F', 'B60', 'RIL', 'I02', 'SPK', 'T11', 'CV50000'],
+                      34: ['A76', 'S-F', 'B10', 'M98', 'Y81', 'B02', 'T11', 'CV10000'],
+                      35: ['A76', 'S-F', 'TAR', 'RI1', 'Y81', 'B02', 'S14', 'CV30000']}
+
+        physical_options = []
+        snow_parameters = []
+
+        for mb in members:
+
+            po, sp = self.convert_options(*allmembers[mb])
+            physical_options.append(po)
+            snow_parameters.append(sp)
+
+        return physical_options, snow_parameters, members
 
     def E2tartes(self, members):
 
@@ -186,6 +241,55 @@ class ESCROC_subensembles(dict):
                       33: ['A76', 'S-F', 'T17', 'RI2', 'I02', 'B92', 'B92', 'CV30000'],
                       34: ['A76', 'S-F', 'T17', 'RIL', 'Y81', 'SPK', 'S14', 'CV50000'],
                       35: ['A76', 'S-F', 'T17', 'RI1', 'Y81', 'SPK', 'B92', 'CV30000']}
+
+        physical_options = []
+        snow_parameters = []
+
+        for mb in members:
+
+            po, sp = self.convert_options(*allmembers[mb])
+            physical_options.append(po)
+            snow_parameters.append(sp)
+
+        return physical_options, snow_parameters, members
+
+    def E2MIPtartes(self, members):
+
+        allmembers = {1: ['V12', 'C13', 'T17', 'RI2', 'Y81', 'SPK', 'T11', 'CV30000'],
+                      2: ['V12', 'C13', 'T17', 'RI1', 'Y81', 'B02', 'S14', 'CV30000'],
+                      3: ['V12', 'C13', 'T17', 'M98', 'Y81', 'SPK', 'S14', 'CV10000'],
+                      4: ['V12', 'F06', 'T17', 'RI2', 'Y81', 'SPK', 'T11', 'CV10000'],
+                      5: ['V12', 'F06', 'T17', 'RI2', 'I02', 'SPK', 'B92', 'CV50000'],
+                      6: ['V12', 'F06', 'T17', 'M98', 'Y81', 'SPK', 'B92', 'CV10000'],
+                      7: ['V12', 'S-F', 'T17', 'RIL', 'Y81', 'SPK', 'T11', 'CV10000'],
+                      8: ['V12', 'S-F', 'T17', 'RI1', 'Y81', 'SPK', 'T11', 'CV50000'],
+                      9: ['V12', 'S-F', 'T17', 'RI1', 'I02', 'B92', 'T11', 'CV50000'],
+                      10: ['V12', 'S-F', 'T17', 'M98', 'I02', 'B02', 'B92', 'CV10000'],
+                      11: ['S02', 'C13', 'T17', 'RIL', 'Y81', 'B92', 'S14', 'CV50000'],
+                      12: ['S02', 'C13', 'T17', 'M98', 'Y81', 'SPK', 'S14', 'CV30000'],
+                      13: ['S02', 'C13', 'T17', 'RIL', 'I02', 'B92', 'S14', 'CV50000'],
+                      14: ['S02', 'C13', 'T17', 'M98', 'I02', 'SPK', 'T11', 'CV10000'],
+                      15: ['S02', 'C13', 'T17', 'RI2', 'Y81', 'B02', 'B92', 'CV50000'],
+                      16: ['S02', 'F06', 'T17', 'RIL', 'I02', 'SPK', 'B92', 'CV30000'],
+                      17: ['S02', 'F06', 'T17', 'RI2', 'Y81', 'B92', 'T11', 'CV10000'],
+                      18: ['S02', 'F06', 'T17', 'RI1', 'I02', 'B92', 'S14', 'CV30000'],
+                      19: ['S02', 'F06', 'T17', 'RI2', 'I02', 'B02', 'B92', 'CV50000'],
+                      20: ['S02', 'F06', 'T17', 'RIL', 'I02', 'SPK', 'T11', 'CV10000'],
+                      21: ['S02', 'F06', 'T17', 'M98', 'I02', 'B92', 'B92', 'CV50000'],
+                      22: ['S02', 'S-F', 'T17', 'RIL', 'I02', 'SPK', 'T11', 'CV50000'],
+                      23: ['S02', 'S-F', 'T17', 'RI2', 'I02', 'B92', 'B92', 'CV30000'],
+                      24: ['A76', 'C13', 'T17', 'RI1', 'Y81', 'SPK', 'T11', 'CV30000'],
+                      25: ['A76', 'C13', 'T17', 'RI2', 'I02', 'B92', 'S14', 'CV50000'],
+                      26: ['A76', 'C13', 'T17', 'RIL', 'I02', 'B02', 'B92', 'CV10000'],
+                      27: ['A76', 'C13', 'T17', 'M98', 'I02', 'SPK', 'T11', 'CV50000'],
+                      28: ['A76', 'C13', 'T17', 'RI1', 'Y81', 'B92', 'B92', 'CV50000'],
+                      29: ['A76', 'C13', 'T17', 'RIL', 'I02', 'B02', 'S14', 'CV50000'],
+                      30: ['A76', 'F06', 'T17', 'M98', 'I02', 'B92', 'T11', 'CV30000'],
+                      31: ['A76', 'F06', 'T17', 'RI1', 'Y81', 'B92', 'T11', 'CV10000'],
+                      32: ['A76', 'F06', 'T17', 'RI2', 'I02', 'B92', 'T11', 'CV10000'],
+                      33: ['A76', 'S-F', 'T17', 'RIL', 'I02', 'SPK', 'T11', 'CV50000'],
+                      34: ['A76', 'S-F', 'T17', 'M98', 'Y81', 'B02', 'T11', 'CV10000'],
+                      35: ['A76', 'S-F', 'T17', 'RI1', 'Y81', 'B02', 'S14', 'CV30000']}
 
         physical_options = []
         snow_parameters = []
