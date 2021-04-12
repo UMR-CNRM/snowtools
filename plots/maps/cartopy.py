eb5cc08dd59b87787c9b173a2cfc285d5be0fb68
radanovicss <sabine.radanovics@meteo.fr>
2021-04-12 15:28:39 +0200

    Output all quantiles in postprocessing in 4seasons task. Some more tests needed.

---
+++ plots/maps/cartopy.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 29 march 2021
+
+@author: radanovics
+
+start collecting resources for plotting maps with cartopy.
+Might be sensitive to the combination of versions of matplotlib and cartopy.
+developed with matplotlib 3.4.0 and cartopy 0.18
+"""
+
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import cartopy.crs as ccrs
+
