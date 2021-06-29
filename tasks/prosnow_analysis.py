c022b7cb0386f1e73f4a1da1f99cf16080432b0f
carlo carmagnola <carlo.carmagnola@meteo.fr>
2021-06-29 17:52:08 +0200

    update 5 prosnow tasks

--- tasks/prosnow_analysis.py
+++ tasks/prosnow_analysis.py
@@ -76,7 +76,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
                     local          = 'FORCING_[datebegin::ymd6h]_[dateend::ymd6h].nc',
                     experiment     = xpid,
                     block          = 'massifs',
-                    geometry       = 'alp',
+                    geometry       = self.conf.vconf,
                     nativefmt      = 'netcdf',
                     model          = 'safran',
                     datebegin      = self.conf.datebegin,
@@ -84,7 +84,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
                     namespace      = 'vortex.multi.fr',
                     date           = '[dateend::ymd]09',
                     vapp           = 's2m',
-                    vconf          = 'alp',
+                    vconf          = self.conf.vconf,
                     fatal          = False,
                 )
                 print(t.prompt, 'in_tb01 =', in_tb01)
@@ -102,7 +102,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
                     local          = 'FORCING_[datebegin::ymd6h]_[dateend::ymd6h].nc',
                     experiment     = xpid,
                     block          = 'massifs',
-                    geometry       = 'alp',
+                    geometry       = self.conf.vconf,
                     nativefmt      = 'netcdf',
                     model          = 'safran',
                     datebegin      = self.conf.datebegin,
@@ -110,7 +110,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
                     namespace      = 'vortex.multi.fr',
                     date           = '[dateend::ymd]06',
                     vapp           = 's2m',
-                    vconf          = 'alp',
+                    vconf          = self.conf.vconf,
                 )
                 print(t.prompt, 'in_tb01 =', in_tb01)
                 print()
@@ -244,7 +244,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
             )
             print(t.prompt, 'in_tb10 =', in_tb10)
             print()
- 
+
             self.sh.title('Toolbox input in_tb11')
             '''11) INPUT -> search for PREP of previous simulation'''
             in_tb11 = toolbox.input(
@@ -390,7 +390,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
             print(t.prompt, 'tb02 =', tb02)
             print()
             tb02.run()
- 
+
             self.sh.title('Toolbox algo alg_tb03')
             '''3) ALGO -> run surfex (Prosnow_Parallel)'''
             alg_tb03 = toolbox.algo(
@@ -434,7 +434,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
                 ),
                 print(t.prompt, 'out_tb01 =', out_tb01)
                 print()
- 
+
                 self.sh.title('Toolbox output out_tb02')
                 '''2) OUTPUT -> prep'''
                 out_tb02 = toolbox.output(
@@ -451,7 +451,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
                 ),
                 print(t.prompt, 'out_tb02 =', out_tb02)
                 print()
- 
+
                 self.sh.title('Toolbox output out_tb03')
                 '''3) OUTPUT -> pro'''
                 out_tb03 = toolbox.output(
@@ -460,6 +460,7 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
                     geometry       = self.conf.geometry,
                     datebegin      = [self.conf.datebegin],
                     dateend        = [self.conf.dateend],
+                    date           = [self.conf.datebegin],
                     nativefmt      = 'netcdf',
                     kind           = 'SnowpackSimulation',
                     model          = 'surfex',
@@ -479,9 +480,11 @@ class Prosnow_Analysis(Task, S2MTaskMixIn):
                     # '''1) Past years'''
 #                     datebegin      = [datebegin],
 #                     dateend        = [dateend],
+#                     date           = [datebegin],
                     # '''2) Real-time'''
                     datebegin      = self.conf.datebegin,
                     dateend        = self.conf.dateend,
+                    date           = self.conf.datebegin,
                     nativefmt      = 'netcdf',
                     kind           = 'MeteorologicalForcing',
                     model          = 's2m',
