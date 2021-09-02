edccfd09aa88b71aea9e346b512519e21aa428ed
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2021-09-02 12:26:16 +0200

    Tests: homogeneize output folder management and limit stdout invasion
    Homogeneize output folder management : autoremove passed tests outputs
    and put all in test_fail
    
    Catch stdout during execution in a file and print this file content only
    in case of test error.

--- tests/test_dev_surfex.py
+++ tests/test_dev_surfex.py
@@ -7,17 +7,29 @@ Created on 11 oct. 2017
 '''
 import unittest
 import os
+import sys
+import shutil
 import datetime
+import tempfile
 from tasks.s2m_command import Surfex_command as s2m
+from tests.export_output import exportoutput
+_here = os.path.dirname(os.path.realpath(__file__))
 
 
 class s2mTest(unittest.TestCase):
     def setUp(self):
-        self.diroutput = os.environ["SNOWTOOLS_CEN"] + "/tests/fail_test/output" + datetime.datetime.today().strftime("%Y%m%d%H%M%S%f")
-        self.path_namelist = os.environ["SNOWTOOLS_CEN"] + "/tests/namelists/"
-        self.namelist = os.environ["SNOWTOOLS_CEN"] + "/tests/namelists/namelist.nam"
-        self.forcingtest = os.environ["SNOWTOOLS_CEN"] + "/DATA/FORCING_test_base.nc"
+        basediroutput = os.path.join(_here, "fail_test")
+        if not os.path.isdir(basediroutput):
+            os.makedirs(basediroutput)
+        prefix = "output" + datetime.datetime.today().strftime("%Y%m%d%H%M%S%f-")
+        self.diroutput = tempfile.mkdtemp(prefix=prefix, dir=basediroutput)
+        self.path_namelist = os.path.join(_here, 'namelists/')
+        self.namelist = os.path.join(_here, 'namelists/namelist.nam')
+        self.forcingtest = os.path.join(_here, "../DATA/FORCING_test_base.nc")
         self.commonoptions = " -o " + self.diroutput + " -f " + self.forcingtest + " -n " + self.namelist + " -g"
+        self.logfile = os.path.join(self.diroutput, 'output.log')
+        with open(self.logfile, 'w') as f:
+            f.write('Log for test {} executed on {}'.format(__file__, datetime.datetime.now()))
 
     def tearDown(self):
         if hasattr(self, '_outcome'):  # Python 3.4+
@@ -31,13 +43,15 @@ class s2mTest(unittest.TestCase):
 
         # Demo:   report short info immediately (not important)
         if not ok:
+            with open(self.logfile, 'r') as f:
+                sys.stderr.write(f.read())
             typ, text = ('ERROR', error) if error else ('FAIL', failure)
             msg = [x for x in text.split('\n')[1:] if not x.startswith(' ')][0]
             print("\n%s: %s\n     %s" % (typ, self.id(), msg))
 
         if ok:
             # Suppression des sous-dossiers de fail_test correspondant aux tests OK
-            os.system("rm -rf " + self.diroutput)
+            shutil.rmtree(self.diroutput)
             # Suppression du dossier fail_test si tous les tests ont réussi (ie dossier vide)
             if os.listdir('fail_test') == []:
                 os.rmdir('fail_test')
@@ -48,10 +62,12 @@ class s2mTest(unittest.TestCase):
 
     def full_run(self, shortcommand):
         command = shortcommand + self.commonoptions
-        s2m(command.split())
+        with exportoutput(self.logfile):
+            s2m(command.split())
 
     def runatcen(self):
         return os.path.isdir("/rd/cenfic2/manto/lafaysse")
+
 
 class s2mTestForcageBase(s2mTest):
 
@@ -105,47 +121,48 @@ class s2mTestForcageBase(s2mTest):
         self.full_run("s2m -b 20101215 -e 20110115")
 
 
-
 class s2mTestForcageImpurete(s2mTest):
 
     def setUp(self):
         super(s2mTestForcageImpurete, self).setUp()
-        self.forcingtest = os.environ["SNOWTOOLS_CEN"] + "/DATA/FORCING_test_impur.nc"
+        self.forcingtest = os.path.join(_here, "../DATA/FORCING_test_impur.nc")
         self.commonoptions = " -o " + self.diroutput + " -f " + self.forcingtest + " -n " + self.namelist + " -g"
-        
+
     def test_impur1(self):
-        # NAM_PREP_ISBA_SNOW     NIMPUR=2, 
+        # NAM_PREP_ISBA_SNOW     NIMPUR=2,
         # NAM_IO_OFFLINE         LSPECSNOW = .TRUE.    NIMPUROF=2      LFORCIMP = .TRUE.
-        # NAM_DIAG_ISBAn         LPROBANDS = .TRUE. 
+        # NAM_DIAG_ISBAn         LPROBANDS = .TRUE.
         # NAM_ISBA_SNOWn         CSNOWRAD='T17'   LATMORAD=.FALSE.
         # NAM_SURF_SNOW_CSTS     XIMPUR_WET(1)=0.e-9   XIMPUR_WET(2)=0.e-9   XIMPUR_DRY(1)=0.e-9   XIMPUR_DRY(2)=0.e-9
         os.system("cp -f " + self.path_namelist + "namelist_impur1.nam " + self.namelist)
         self.full_run("s2m -b 20171215 -e 20180115")
 
     def test_impur2(self):
-        # NAM_PREP_ISBA_SNOW     NIMPUR=1, 
+        # NAM_PREP_ISBA_SNOW     NIMPUR=1,
         # NAM_IO_OFFLINE         LSPECSNOW = .TRUE.    NIMPUROF=1      LFORCIMP = .FALSE.
-        # NAM_DIAG_ISBAn         LPROBANDS = .TRUE. 
+        # NAM_DIAG_ISBAn         LPROBANDS = .TRUE.
         # NAM_ISBA_SNOWn         CSNOWRAD='T17'   LATMORAD=.FALSE.
         # NAM_SURF_SNOW_CSTS     XIMPUR_WET(1)=2.e-9   XIMPUR_WET(2)=2.e-9   XIMPUR_DRY(1)=2.e-9   XIMPUR_DRY(2)=2.e-9
         os.system("cp -f " + self.path_namelist + "namelist_impur2.nam " + self.namelist)
         self.full_run("s2m -b 20171215 -e 20180115")
 
+
 class s2m2DTest(s2mTest):
 
     def setUp(self):
         super(s2m2DTest, self).setUp()
-        self.forcingtest = os.environ["SNOWTOOLS_CEN"] + "/DATA/FORCING_test_2d.nc"
-        self.namelist = os.environ["SNOWTOOLS_CEN"] + "/DATA/OPTIONS_test_2d.nam"
-        # If the test is run at CEN, it can run PGD with available databases. Otherwise, we do not test the PGD step and only take a PGD test file.
+        self.forcingtest = os.path.join(_here, "../DATA/FORCING_test_2d.nc")
+        self.namelist = os.path.join(_here, "../DATA/OPTIONS_test_2d.nam")
+        # If the test is run at CEN, it can run PGD with available databases.
+        # Otherwise, we do not test the PGD step and only take a PGD test file.
         if not self.runatcen():
             os.makedirs(self.diroutput+"/prep")
-            os.symlink(os.environ["SNOWTOOLS_CEN"] + "/DATA/PGD_test_2d.nc", self.diroutput+"/prep/PGD.nc")
+            pgd = os.path.join(_here, "../DATA/PGD_test_2d.nc")
+            os.symlink(pgd, self.diroutput+"/prep/PGD.nc")
         self.commonoptions = " -o " + self.diroutput + " --grid -f " + self.forcingtest + " -n " + self.namelist + " -g"
 
     def test_2d_ign(self):
         self.full_run("s2m -b 20150101 -e 20150201")
-
 
 
 if __name__ == "__main__":
