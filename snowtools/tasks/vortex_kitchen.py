bc3543ebb41464db29479baad990790e7d51bd56
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2022-01-13 11:43:41 +0100

    BUG fix : SURFEX conf variable not used in SAFRAn executions

--- snowtools/tasks/vortex_kitchen.py
+++ snowtools/tasks/vortex_kitchen.py
@@ -348,7 +348,8 @@ class Vortex_conf_file(object):
         self.set_field("DEFAULT", 'nprocs', self.options.ntasks)
         self.set_field("DEFAULT", 'openmp', 1)
         self.set_field("DEFAULT", 'geometry', self.options.vconf)
-        self.set_field("DEFAULT", 'addmask', self.options.addmask)
+        if self.options.addmask:
+            self.set_field("DEFAULT", 'addmask', self.options.addmask)
 
     def escroc_variables(self):
 
