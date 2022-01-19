1dbf5d87e5633f7b9f3305693f97c4f808ae66f9
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2022-01-19 15:56:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,46 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import os
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+
+PYTHON_MIN_VERSION = (3, 6)
+
+
+# Get the full list of packages available
+def get_package_list(basename, basepath):
+    r = [basename]
+    for elem in os.listdir(basepath):
+        if (len(elem) > 2 and elem[0:2] != '__') and \
+                os.path.isdir(os.path.join(basepath, elem)) and \
+                os.path.isfile(os.path.join(basepath, elem, '__init__.py')):
+            r += get_package_list(basename + '.' + elem, os.path.join(basepath, elem))
+    return r
+
+
+def package_discover(package_name):
+    return get_package_list(package_name, os.path.join(HERE, package_name))
+
+
+snowtools_packages = package_discover('snowtools')
+bronx_packages = package_discover('bronx')
+all_packages = snowtools_packages + bronx_packages
+all_packages_data = {elem: ['*'] for elem in all_packages}
+all_packages_data['snowtools'] = [
+        'tests/test_soda/*',
+        'tests/namelists/*',
+        'conf/*',
+        'DATA/*',
+        'DATA/OPER/*',
+        'plots/logos/*',
+        'tasks/oper/iga/conf/*',
+        ]
 
 setup(
         name='Snowtools',
         version='2.0.0',
-        description="Snowtools is a recommended python package to pre-process, run and post-process simulations with the SURFEX-Crocus snowpack model",
+        description="Snowtools is a recommended python package to pre-process, run and post-process simulations with the SURFEX-Crocus snowpack model.",
         long_description="""
         Snowtools is a series of mostly python scripts that are designed to make our life simpler
         in terms of pre- and post-processing of SURFEX-Crocus snow model.
@@ -15,15 +50,10 @@ setup(
         snowpack modelling on a Linux environment. The package does not include any meteorological
         or snow data. It must be used in association with the SURFEX project
         """,
-        maintainer='Snowtools team, leaded by Matthieu Lafaysse',
+        maintainer='Snowtools team, leaded by Matthieu Lafaysse, Meteo-France',
         url='https://opensource.umr-cnrm.fr/projects/snowtools_git/wiki/Procedure_for_new_users',
-        package_dir={
-            'snowtools': 'snowtools',
-            'bronx': 'bronx',
-            },
-        # package_data={
-        #     'snowtools.DATA': 'snowtools/DATA/*',
-        #     },
+        packages=all_packages,
+        package_data=all_packages_data,
         classifiers=[
             'Operating System :: POSIX :: Linux',
 
@@ -56,4 +86,5 @@ setup(
              'xarray',
              'cartopy',
             ],
+        python_requires='>=' + '.'.join(str(n) for n in PYTHON_MIN_VERSION),
         )
