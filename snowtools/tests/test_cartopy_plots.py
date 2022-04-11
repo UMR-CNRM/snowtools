235a5419e4fa82f88331e7fd5daefc07e9c218f8
radanovicss <sabine.radanovics@meteo.fr>
2022-04-11 18:08:16 +0200

    Testbase migration cenfic2 -> cenfic3
    TESTBASE_DIR variable introduced in DATA/__init__ pointing to the testbase on cenfic3.
    cenfic2 -> cenfic3 for runatcen methods in tests
    index.html in the Manual_tests is now created when test_cartopy_plots.py is run based on index_template.html and TESTBASE_DIR variable.

--- snowtools/tests/test_cartopy_plots.py
+++ snowtools/tests/test_cartopy_plots.py
@@ -9,11 +9,18 @@ import numpy as np
 from datetime import datetime
 from snowtools.utils.prosimu import prosimu
 from snowtools.plots.maps import cartopy
+from snowtools.DATA import TESTBASE_DIR
 from netCDF4 import Dataset
 import os
 
+
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-TEST_DATA_DIR = "/rd/cenfic2/manto/viallonl/testbase/PRO"
+TEST_DATA_DIR = os.path.join(TESTBASE_DIR, "PRO")
+# read the index file template and insert the testbase directory
+index = open(os.path.join(THIS_DIR, "Manual_tests", "index_template.html")).read().format(testbase=TESTBASE_DIR)
+# write the index.html file with the right testbase paths
+with open(os.path.join(THIS_DIR, "Manual_tests", "index.html"), 'w') as outfile:
+    outfile.write(index)
 
 
 @unittest.skipIf(not os.path.isfile(os.path.join(TEST_DATA_DIR, "postproc", "grid_postproc_2021041112.nc")),
