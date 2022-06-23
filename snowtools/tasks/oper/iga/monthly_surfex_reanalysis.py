2bb863be94b57f970283d1e3b4dbc35dac7e6e6f
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2022-06-23 17:18:21 +0200

    Merge IGA modifications
    Karine ne met pas de code dans le script "extract_for_reanalysis.py" et
    utilise la méthode "refill" directement dans la classe définie dans la
    "tache" recextfiles_monthly.py.

--- snowtools/tasks/oper/iga/monthly_surfex_reanalysis.py
+++ snowtools/tasks/oper/iga/monthly_surfex_reanalysis.py
@@ -36,6 +36,87 @@ class Monthly_Surfex_Reanalysis(S2MTaskMixIn, OpTask):
 
     filter_execution_error = S2MTaskMixIn.s2moper_filter_execution_error
 
+    def refill(self):
+
+        t = self.ticket
+
+        if 'refill' in self.steps:
+
+            datebegin, dateend = self.get_period()
+            rundate_prep, alternate_rundate_prep = self.get_rundate_prep()
+
+            with op.InputReportContext(self, t):
+
+                # This product is written in BDPE once a year by ensemble_surfex_tasks_bdpe.py and read here once a month.
+                self.sh.title('Toolbox input tb01wi')
+                tb01wi = toolbox.input(
+                    role           = 'SnowpackInitForMonthlyReanalysis',
+                    local          = 'PREP.nc',
+                    block          = 'prep',
+                    experiment     = self.conf.xpid,
+                    geometry       = self.conf.geometry,
+                    datevalidity   = datebegin,
+                    date           = rundate_prep,
+                    member         = 35,
+                    namespace      = 'bdpe.archive.fr',
+                    bdpeid         = self.conf.num_bdpe_initrea[self.conf.xpid],
+                    intent         = 'inout',
+                    nativefmt      = 'netcdf',
+                    kind           = 'PREP',
+                    model          = 'surfex',
+                    fatal          = False,
+                    cutoff         = 'assimilation'
+                ),
+                print((t.prompt, 'tb01wi =', tb01wi))
+                print()
+
+                # Alternates (3 previous days)
+                for i, alternate_prep in enumerate(alternate_rundate_prep):
+
+                    fatal = i == len(alternate_rundate_prep) - 1
+
+                    tb01wi = toolbox.input(
+                        alternate='SnowpackInitForMonthlyReanalysis',
+                        local='PREP.nc',
+                        block='prep',
+                        experiment=self.conf.xpid,
+                        geometry=self.conf.geometry,
+                        datevalidity=datebegin,
+                        date=alternate_prep[0],
+                        member=35,
+                        namespace='bdpe.archive.fr',
+                        bdpeid=self.conf.num_bdpe_initrea[self.conf.xpid],
+                        intent='inout',
+                        nativefmt='netcdf',
+                        kind='PREP',
+                        model='surfex',
+                        fatal=fatal,
+                        cutoff='assimilation'
+                    ),
+                    print((t.prompt, 'tb01wi =', tb01wi))
+                    print()
+
+            with op.OutputReportContext(self, t):
+
+                self.sh.title('Toolbox output tb01wo')
+                tb01wo = toolbox.output(
+                        role           = 'SnowpackInit',
+                        local          = 'PREP.nc',
+                        block          = 'prep',
+                        experiment     = self.conf.xpid,
+                        geometry       = self.conf.geometry,
+                        datevalidity   = datebegin,
+                        member         = 35,
+                        namespace      = 'vortex.cache.fr',
+                        nativefmt      = 'netcdf',
+                        kind           = 'PREP',
+                        model          = 'surfex',
+                        fatal          = True,
+                        cutoff         = 'assimilation'
+                ),
+                print((t.prompt, 'tb01wo =', tb01wo))
+                print()
+
     def process(self):
 
         t = self.ticket
@@ -99,7 +180,7 @@ class Monthly_Surfex_Reanalysis(S2MTaskMixIn, OpTask):
                 experiment     = self.conf.xpid,
                 geometry       = self.conf.geometry,
                 datevalidity   = datebegin,
-                date           = rundate_prep,
+#                date           = rundate_prep,
                 member         = 35,
                 namespace      = self.conf.namespace_in,
                 intent         = 'inout',
