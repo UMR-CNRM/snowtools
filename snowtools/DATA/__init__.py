5fc16bc2d6c6c3c4a70b3c09f9a896aad550dc1f
Matthieu Lafaysse <matthieu.lafaysse@meteo.fr>
2022-07-08 12:08:24 +0200

    bug fix from 7bda0236 for vortex jobs

--- snowtools/DATA/__init__.py
+++ snowtools/DATA/__init__.py
@@ -29,7 +29,11 @@ PGD files at CEN
 
 LUSTRE_NOSAVE_DIR = '/cnrm/mrns/users/NO_SAVE'
 
-LUSTRE_NOSAVE_USER_DIR = os.path.join('/cnrm/mrns/users/NO_SAVE', os.getlogin())
+try:
+    LUSTRE_NOSAVE_USER_DIR = os.path.join('/cnrm/mrns/users/NO_SAVE', os.getlogin())
+except OSError:
+    # os.getlogin not defined in a vortex job but we don't care because this variable is for sxcen.
+    LUSTRE_NOSAVE_USER_DIR = None
 
 CARTOPY_DIR = '/rd/cenfic3/manto/radanovicss/CartopyData'
 # CARTOPY_DIR = os.path.join(LUSTRE_NOSAVE_USER_DIR, 'CartopyData')  # for sxcen
