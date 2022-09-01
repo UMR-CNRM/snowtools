UMR-CNRM/snowtools

commit 884f5d3e9e1795c16c1e2110af87146a4ee93918

prosimu: provide a path with wildcard to load a set of files with MFDataset

snowtools/utils/prosimu.py
@@ -16,9 +16,10 @@ import sys
 import netCDF4
 import six
 import numpy as np
+import glob
 
 from snowtools.utils.FileException import FileNameException, DirNameException, FileOpenException, VarNameException, \
-        TimeException, MultipleValueException
+    TimeException, MultipleValueException
 from snowtools.utils.S2M_standard_file import StandardCROCUS
 
 try:
@@ -63,7 +64,11 @@ class prosimu():
         cache - utile lorsque de grands nombre d'accès à la même variable sont
         nécessaires
         """
-        if type(path) is list:
+        # BC add the possibility to give wildcards to prosimu
+        glob_path = glob.glob(path)
+        if type(path) is list or len(glob_path) > 1:
+            if type(path) is not list:
+                path = sorted(glob_path)
             for fichier in path:
                 if not os.path.isfile(fichier):
                     raise FileNameException(fichier)

commit fa574c5616f08557f65ed09145b32c26ecd469b2

bug fix (post-processing writing was broken by commits 884f5d3e-da089c49 )

snowtools/utils/prosimu.py
@@ -66,7 +66,12 @@ class prosimu():
         """
         # BC add the possibility to give wildcards to prosimu
         if type(path) is str:
-            glob_path = glob.glob(path)
+            if openmode == 'w':
+                glob_path = [path]
+            else:
+                glob_path = glob.glob(path)
+            if len(glob_path) == 0:
+                raise FileNameException(path)
             path = sorted(glob_path)
             # otherwise path is already a list.
 