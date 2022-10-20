cefbd83586ac6efc14ec06ef2fc34e0da6720fea
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2022-10-20 15:52:13 +0200

    DBUG operationnal chain
    The rapatrie task is a refill task, outputs must also be in the refill
    otherwise they are not called

--- snowtools/tasks/oper/iga/rapatrie.py
+++ snowtools/tasks/oper/iga/rapatrie.py
@@ -142,11 +142,7 @@ class Reanalyses(OpTask, S2MTaskMixIn):
                 print(t.prompt, 'tb02b =', tb02b)
                 print()
 
-            #TODO :prévoir de regénérer les éventuels guess manquants
-
-        if 'late-backup' in self.steps:
-
-            with op.OutputReportContext(self, t):
+                #TODO :prévoir de regénérer les éventuels guess manquants
 
                 # Mise à jour de l'archive,
                 # TODO :
