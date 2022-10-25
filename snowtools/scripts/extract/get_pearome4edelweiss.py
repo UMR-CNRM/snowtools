e066d4120742fe0debe4e9e90e4df494e0f20c53
radanovicss <sabine.radanovics@meteo.fr>
2022-10-25 11:34:18 +0200

    adapt pearome4edelweiss to last version of extracthendrix

--- snowtools/scripts/extract/get_pearome4edelweiss.py
+++ snowtools/scripts/extract/get_pearome4edelweiss.py
@@ -9,58 +9,65 @@ Created on Wed Oct 13 15:04:27 2021
 import os
 import extracthendrix.configreader as configreader
 from bronx.stdtypes.date import Date
+from datetime import datetime
 from extracthendrix.config.variables import pearome
 from snowtools.DATA import LUSTRE_NOSAVE_USER_DIR
 
 config_user = dict(
 
     #  Where you want to store the outputs
-    folder=os.path.join(LUSTRE_NOSAVE_USER_DIR,'PEAROME'),
+    work_folder=os.path.join(LUSTRE_NOSAVE_USER_DIR,'PEAROME'),
     native_files_folder=os.path.join(LUSTRE_NOSAVE_USER_DIR,'PEAROME','_native_files_'),
     cache_folder=os.path.join(LUSTRE_NOSAVE_USER_DIR,'PEAROME','_cache_'),
 
 
     # Models are defined in the models.ini file
-    model_name='PEAROME',
-    members=range(1, 17),
+    model='PEAROME',
+    members=range(1, 2),
     # The domain can be defined by its name or its coordinates
     # Existing domain can be found in the config_fa2nc.py file
     domain="alp",
 
     # Variables to extract and to store in the netcdf file
     # Variable are defined in the config_fa2nc.py file
-    # variables_nc=[pearome.Tair, pearome.Qair, pearome.Wind, pearome.Wind_DIR, pearome.Psurf,
-    #               pearome.Rainf, pearome.Snowf, pearome.LWdown, pearome.SWdown, pearome.NEB,
-    #               pearome.HUMREL, pearome.isoZeroAltitude, pearome.isowetbt0, pearome.isowetbt1,
-    #               pearome.isowetbt1_5],
-    variables_nc=[pearome.ZS],
+    # variables=['Tair', 'Qair', 'Wind', 'Wind_DIR', 'Psurf',
+    #               'Rainf', 'Snowf', 'LWdown', 'SWdown', 'NEB',
+    #               'HUMREL', 'isoZeroAltitude', 'isowetbt0', 'isowetbt1',
+    #               'isowetbt1_5'],
+    # variables=['Psurf', 'prmsl', 'P3d', 't3d', 'papt3d', 'r3d', 'clwc3d', 'ciwc10m', 'ciwc100m',
+    #            'cswc10m', 'cswc100m', 'crwc10m', 'crwc100m', 't2m', 'Qair', 'u10m', 'v10m',
+    #            'Rainf', 'Snowf', 'refl0m', 'reflfactorsnow', 'HUMREL', 'isoZeroAltitude',
+    #            'isowetbt0', 'isowetbt1', 'isowetbt1_5', 'sd', 'avgptype', 'maxptype'],
+    variables=['ZS'],
 
     # "local" if the FA file are on your computer or "hendrix" otherwise
     getter="hendrix",
 
     # For pre-staging and sending mail during (your mail = destination) extraction
-    email_adress="sabine.radanovics@meteo.fr",
+    email_address="sabine.radanovics@meteo.fr",
 
     # datetime(year, month, day)
-    date_start=Date(2020, 3, 1),
-    date_end=Date(2020, 3, 1),
+    start_date=datetime(2022, 4, 1),
+    end_date=datetime(2022, 4, 1),
 
     # Analysis hour
-    analysis_hour=3,
+    run=3,
 
     # Delta between terms
-    delta_terms=1,
+    delta_t=1,
 
     # Term in hour after analysis
     start_term=0,  # Default: 6
     end_term=0,  # Default: 6+24 = 30
+    dtype='64bits',
+    make_surfex=False,
 
     # todo: concatenation mode : "timeseries" or "forecast"
-    concat_mode="forecast",
+    groupby=("forecast",),
 
     # How to group the netcdf files: ["month", "year", "all"] for timeseries,
     # and  ["deterministic", "ensemble"] for forecasts
-    final_concatenation="all"
+    # final_concatenation="all"
 )
 # e = eh.Extractor(config_user)
 # ref 'first_i': np.intp(900), 'last_i': np.intp(1075), 'first_j': np.intp(525), 'last_j': np.intp(750)}
@@ -71,6 +78,6 @@ config_user = dict(
 # print(e.ij2latlon(1090, 1100, 740, 750))
 
 # e.download()
-e = configreader.apply_config_user(config_user)
+e = configreader.execute(config_user)
 # arome_extr = eh.readers.AromeHendrixReader(native_files_folder=config_user['folder'], model=config_user['model_name'],
 #                                           runtime=config_user['analysis_hour'])
