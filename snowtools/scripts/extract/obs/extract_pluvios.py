f4f4200b670f9e9ee208b98c6a503b9f5ec39a36
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2023-10-02 10:40:54 +0200

    Add observation extraction over the Pyrenees

--- snowtools/scripts/extract/obs/extract_pluvios.py
+++ snowtools/scripts/extract/obs/extract_pluvios.py
@@ -30,20 +30,26 @@ if __name__ == "__main__":
     begin = Date(datedeb)
     end = Date(datefin)
 
+    latmin = dict(alp=44.1, pyr=41.5)
+    latmax = dict(alp=46.45, pyr=44.0)
+    lonmin = dict(alp=5.4, pyr=-2.5)
+    lonmax = dict(alp=7.2, pyr=4.0)
+
 # 1. RR horaires
 #-----------------
-    question1 = question(
-        listvar=["h.dat", "h.num_poste", "poste.nom_usuel", "poste.lat_dg", "poste.lon_dg", "poste.alti", "h.rr1", "hist_reseau_poste.reseau_poste"],
-        table='H',
-        listjoin=['POSTE ON H.NUM_POSTE=POSTE.NUM_POSTE ', 
-            'HIST_RESEAU_POSTE ON HIST_RESEAU_POSTE.NUM_POSTE=POSTE.NUM_POSTE'],
-        listconditions = [
-            "hist_reseau_poste.datdeb<=h.dat and (hist_reseau_poste.datfin>=h.dat or hist_reseau_poste.datfin is null) and h.rr1 is not null",
-            #"poste.lat_dg<=47.0 and poste.lat_dg>=42.75 and poste.lon_dg<=9. and poste.lon_dg>=4.5",
-            "poste.lat_dg<=46.45 and poste.lat_dg>=44.1 and poste.lon_dg<=7.2 and poste.lon_dg>=5.4",
-            ],
-        period=[datedeb, datefin],
-        listorder=['h.dat', 'h.num_poste'],
-        )
-    question1.run(outputfile=f"obs_horaires_RR_{begin.ymd}_{end.ymd}.csv", header=['date', 'num_poste', 'nom', 'lat', 'lon', 'alti', 'rr', 'reseau_poste'])
+    for domain in ['alp', 'pyr']:
+        question1 = question(
+            listvar=["h.dat", "h.num_poste", "poste.nom_usuel", "poste.lat_dg", "poste.lon_dg", "poste.alti", "h.rr1", "hist_reseau_poste.reseau_poste"],
+            table='H',
+            listjoin=['POSTE ON H.NUM_POSTE=POSTE.NUM_POSTE ', 
+                'HIST_RESEAU_POSTE ON HIST_RESEAU_POSTE.NUM_POSTE=POSTE.NUM_POSTE'],
+            listconditions = [
+                "hist_reseau_poste.datdeb<=h.dat and (hist_reseau_poste.datfin>=h.dat or hist_reseau_poste.datfin is null) and h.rr1 is not null",
+                #"poste.lat_dg<=47.0 and poste.lat_dg>=42.75 and poste.lon_dg<=9. and poste.lon_dg>=4.5",
+                f"poste.lat_dg<={latmax[domain]} and poste.lat_dg>={latmin[domain]} and poste.lon_dg<={lonmax[domain]} and poste.lon_dg>={lonmin[domain]}",
+                ],
+            period=[datedeb, datefin],
+            listorder=['h.dat', 'h.num_poste'],
+            )
+        question1.run(outputfile=f"obs_horaires_RR_{begin.ymd}_{end.ymd}_{domain}.csv", header=['date', 'num_poste', 'nom', 'lat', 'lon', 'alti', 'rr', 'reseau_poste'])
 
