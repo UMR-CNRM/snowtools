c934f561c43b9fe51604d5524b6fd8150aeb2c0b
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-05-31 19:28:38 +0200

    Increase font size and change default min colorbar value

--- snowtools/plots/maps/plot2D.py
+++ snowtools/plots/maps/plot2D.py
@@ -10,15 +10,17 @@ Collection of functions to plot 2D fields.
 '''
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 
 def plot_field(field, savename, vmin=None, vmax=None, cmap=plt.cm.YlGnBu, addpoint=None):
+    matplotlib.rcParams.update({'font.size': 22})
     fig, ax = plt.subplots(figsize=(12 * np.shape(field)[1] / np.shape(field)[0], 10))
     if vmax is None:
         vmax = np.max(np.abs(field))
     if vmin is None:
-        vmin = 0
+        vmin = -vmax
     cmap.set_bad('grey', 1.)
     cml = field.plot(cmap=cmap, vmin=vmin, vmax=vmax)
     cml.set_edgecolor('face')
