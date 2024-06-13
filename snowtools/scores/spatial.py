37a0902cc82aa8dc704b3bac8cfa1e96122c20eb
radanovicss <sabine.radanovics@meteo.fr>
2024-06-13 16:38:33 +0200

    two additional fuzzy scores added : ACC and PAG

--- snowtools/scores/spatial.py
+++ snowtools/scores/spatial.py
@@ -9,13 +9,87 @@ based on code from Ange Haddjeri thesis.
 import numpy as np
 import xarray as xr
 from scipy.signal import convolve2d
+from scipy.interpolate import interpn, interp1d
 from abc import ABC, abstractmethod
 from snowtools.scores.list_scores import SpatialScoreFile
+from snowtools.utils.prosimu import prosimu_xr
+from snowtools.scores import crps
+
+def call_crps(fc, obs):
+    """
+    calls the crps subroutine
+    :param fc: array or list of forcasted values (ensemble members)
+    :param obs: array of observations or reference
+    :return: crpsval
+    """
+    fc = fc.reshape(-1)
+    obs = obs.reshape(-1)
+    fc = fc[~np.isnan(fc)]
+    obs = obs[~np.isnan(obs)]
+
+    crpsval = crps.crps(fc, obs, len(fc), len(obs))
+    return crpsval
+
+def crps_spatial_scipy(fc, obs):
+    """
+    a lot faster than call_crps, but attention: interp1d will be deprecated and interpn doesn't deal with ties.
+    :param fc: array of forecast
+    :type fc: np.ndarray
+    :param obs: array of observation or reference
+    :type obs: np.ndarray
+    :return: crps
+    """
+    fc = fc.reshape(-1)
+    fc.sort()
+    # print(fc)
+    obs = obs.reshape(-1)
+    obs = obs[~np.isnan(obs)]
+    obs.sort()
+    # print(obs)
+    ss = np.arange(0, 400, .5)
+    cdf_fc = interp1d(fc, np.linspace(0, 1, num=len(fc)), kind='nearest',
+                                          fill_value="extrapolate")
+    # print(cdf_fc(ss))
+    cdf_obs = interp1d(obs, np.linspace(0, 1, num=len(obs)), kind='nearest',
+                      fill_value="extrapolate")
+    # print(cdf_obs(ss))
+    crps = np.trapz((cdf_fc(ss) - cdf_obs(ss)) ** 2, x=ss)
+    return crps
+
+
+def crps_spatial_scipy_interpn(fc, obs):
+    """
+    crps calculation using scipy interpolation and numpy integration methods.
+    Attention: interpn seems not to work with ties.
+
+    :param fc: array of forecast
+    :type fc: np.ndarray
+    :param obs: array of observation or reference
+    :type obs: np.ndarray
+    :return: crps
+    """
+    fc = fc.reshape(-1)
+    fc.sort()
+    fct = (fc,)
+    obs = obs.reshape(-1)
+    obs.sort()
+    obst = (obs,)
+    ss = np.arange(0, 400, .5)
+
+    cdf_fc = interpn(fct, np.linspace(0, 1, num=len(fc)), ss, method="nearest",
+                     bounds_error=False, fill_value=None)
+    cdf_obs = interpn(obst, np.linspace(0, 1, num=len(obs)), ss, method="nearest",
+                      bounds_error=False, fill_value=None)
+    crps = np.trapz((cdf_fc - cdf_obs)**2, x=ss)
+    return crps
 
 
 def mincoverage_small(fc, obs, kl, threshold_lower, threshold_increment, perzone=None):
     """
-    Calculate POD, FAR, TS, ETS, HK between two fields using a given neighborhood size kl, and
+    Calculate POD (probability of detection), FAR (false alarm ratio), TS (threat score),
+     ETS (equitable threat score), HK (Hanssen & Kuipper’s Skill Score, True Skill
+      Statistic (TSS) or Pierce’s Skill Score),
+      ACC (accuracy), PAG (post agreement) between two fields using a given neighborhood size kl, and
     a given threshold and threshold increment to define an event.
 
     :param fc: forecast field
@@ -30,7 +104,7 @@ def mincoverage_small(fc, obs, kl, threshold_lower, threshold_increment, perzone
     :param perzone: minimum fraction of values needed to be present in the neighborhood in order to take
         the pixel into account for score calculation. Default None will be set to 1 / (kl * kl)
     :type perzone: float
-    :return: POD, FAR, TS, ETS, HK
+    :return: POD, FAR, TS, ETS, HK, ACC, PAG
     """
     if perzone is None:
         perzone = 1 / (kl * kl)
@@ -69,19 +143,21 @@ def mincoverage_small(fc, obs, kl, threshold_lower, threshold_increment, perzone
 
     POD = hit / (hit + mis)
     FAR = fa / (hit + fa)
+    PAG = hit / (hit + fa)
     TS = hit/(hit+mis+fa)
     ETS = (hit-hitrand)/(hit+mis+fa-hitrand)
     HK = hit-fa
+    ACC = hit + (corej / (hit + mis)) + (hit + corej + fa + mis)
 
-    return POD, FAR, TS, ETS, HK
+    return POD, FAR, TS, ETS, HK, ACC, PAG
 
 
 class SpatialScores(ABC):
-    def __init__(self, fc_filenames, list_of_experiments, obs_filename, list_of_kernels, list_of_thresholds,
+    def __init__(self, fc_filenames, list_of_experiments, obs_filename, varname, list_of_kernels, list_of_thresholds,
                  list_of_threshold_increments, per_zone=None, score_file=True, score_file_name="spatial_scores.nc",
                  perf_plot=True, perf_plot_file="perfdiag.png"):
-        self.fc_data = self.get_fc_data(fc_filenames, list_of_experiments)
-        self.obs_data = self.get_obs_data(obs_filename)
+        self.obs_data = self.get_obs_data(obs_filename, varname)
+        self.fc_data = self.get_fc_data(fc_filenames, list_of_experiments, varname)
         self.score_ds = SpatialScoreFile(list_of_experiments, list_of_kernels, list_of_thresholds,
                                          list_of_threshold_increments)
         self.experiments = list_of_experiments
@@ -95,13 +171,15 @@ class SpatialScores(ABC):
         self.per_zone = per_zone
 
     @abstractmethod
-    def get_fc_data(self, filenames, list_of_experiments):
+    def get_fc_data(self, filenames, list_of_experiments, varname):
         """
         read forecast data
 
         :param list_of_experiments: list of experiment labels
         :param filenames: list of forecast file names (paths)
-        :return: dict with experiment labels as keys and np.arrays of forecast fields as values
+        :param varname: variable name in data set
+        :type varname: str
+        :return: dict with experiment labels as keys and xarray.DataArrays of forecast fields as values
         """
         pass
 
@@ -129,11 +207,14 @@ class SpatialScores(ABC):
     # obs = maskgf(Q.DSN_T_ISBA)
 
     @abstractmethod
-    def get_obs_data(self, filename):
+    def get_obs_data(self, filename, varname):
         """
         read observation data
+
         :param filename: observation filename
-        :return: np.array with observation field
+        :param varname: variable name in data set
+        :type varname: str
+        :return: xarray.DataArray with observation field
         """
         pass
 
@@ -166,15 +247,70 @@ class SpatialScores(ABC):
                         self.score_ds["FAR"].data[iexp, ik, ithres], \
                         self.score_ds["CSI"].data[iexp, ik, ithres], \
                         self.score_ds["ETS"].data[iexp, ik, ithres], \
-                        self.score_ds["HK"].data[iexp, ik, ithres], = mincoverage_small(self.fc_data[exp],
-                                                                                        self.obs_data, kernel,
+                        self.score_ds["HK"].data[iexp, ik, ithres], \
+                        self.score_ds["ACC"].data[iexp, ik, ithres], \
+                        self.score_ds["PAG"].data[iexp, ik, ithres] = mincoverage_small(self.fc_data[exp].data,
+                                                                                        self.obs_data.data, kernel,
                                                                                         thres, thres_inc,
                                                                                         self.per_zone)
 
+    def make_spatial_probability_score(self):
+        """
+        calculate spatial probability score (spatial CRPS) and write it in a new variable in self.score_ds
+        """
+        self.score_ds['SPS'] = xr.DataArray(np.empty((len(self.experiments))),
+                                   dims=('experiment'),
+                                   attrs={'long_name': 'Spatial Probability Score'})
+        self.score_ds['SPS'].data.fill(np.nan)
+        for iexp, exp in enumerate(self.experiments):
+            self.score_ds['SPS'].data[iexp] = crps_spatial_scipy(self.fc_data[exp].data, self.obs_data.data)
+
     def process(self):
         self.make_fuzzy_scores()
+        self.make_spatial_probability_score()
         if self.score_file:
             self.score_ds.to_netcdf(self.score_file_name)
+
+
+class ProVsPleiade(SpatialScores):
+    """
+    class for comparing simulations data with a pleiade image.
+    """
+    def get_fc_data(self, filenames, list_of_experiments, varname):
+        outdict = {}
+        for exp, path in zip(list_of_experiments, filenames):
+            fc_ds = prosimu_xr(path)
+            outdict[exp] = fc_ds.dataset[varname]
+        return outdict
+
+
+    def get_obs_data(self, filename, varname):
+        """
+        read pleiade observation data
+
+        :param filename: file name
+        :return: xarray data array
+        """
+        obs_ds = prosimu_xr(filename)
+        obs_ds.dataset = obs_ds.dataset.rename(x="xx", y="yy")
+        return obs_ds.dataset[varname]
+
+        #     time = obs_ds.readtime()
+        #     snowheight = obs_ds.read_var('DSN_T_ISBA')
+        #     x = obs_ds.read_var('x')
+        #     y = obs_ds.read_var('y')
+        #
+        # return dict(time=time, x=x, y=y, values=snowheight)
+
+    def select_fc_at_obs(self):
+        for exp in self.fc_data.keys():
+            self.fc_data[exp] = self.fc_data[exp].sel(time=self.obs_data['time'], xx=self.obs_data['xx'],
+                                                      yy=self.obs_data['yy'])
+
+    def apply_mask(self):
+        pass
+
+
 
 ##################################
 # structural similarity
