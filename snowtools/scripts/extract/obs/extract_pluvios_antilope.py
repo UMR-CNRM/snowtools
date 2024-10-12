87c75ae1f31da206e73e67b3642d144dc74c3fdb
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-10-12 15:10:02 +0200

    Merge extraction script from branch dev_mv_new_vortex_tasks

--- snowtools/scripts/extract/obs/extract_pluvios_antilope.py
+++ snowtools/scripts/extract/obs/extract_pluvios_antilope.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 
 # Extraction des données d'observation depuis la BDCLIM
-# Matthieu Lafaysse 10 sept 2014
-# Script simplifié pour obtenir un format obs.csv depuis la BDCLIM
+# Author : Matthieu Vernay
+# Script simplifié pour extraire les pluvios utilisés par la LE ANTILOPE.
+
+# Doc BDClim : http://dpnet.meteo.fr/DCLIM/doc/climsol.html
+# Doc réseaux postes : http://gedmf/share/page/document-details?nodeRef=workspace://SpacesStore/b31a01c0-d252-4e3e-ba86-9e1c25b5d0d5
 
 import sys
 
@@ -11,37 +14,65 @@ from snowtools.scripts.extract.obs.bdquery import question
 
 
 def usage():
-    print("USAGE extract_pluvios_antilope.py datedeb datefin")
+    print("USAGE extract_pluvios_antilope.py domain [datedeb datefin]")
     print("format des dates : YYYYMMDDHH")
     sys.exit(1)
 
 
+domain_map = dict(
+    alp            = (47., 41., 4., 10.),
+    MercantourTheo = (44.7, 43.2, 6.3, 8.),
+)
+
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
+    domain = sys.argv[1]
+    latmax, latmin, lonmin, lonmax = domain_map[domain]
+    listconditions = [
+        # Apply ANTILOPE filter for "reseau_poste" (Cf mail from O.Laurantin on may 2022)
+        "hist_reseau_poste.reseau_poste not in ('00','01','02','03','04','09','40','42','43','44','50','51','52','53',"
+        "'54','83','90','91','92','93','94','95','96','97')",
+        # Filter out EDF-nivo observations
+        "not nom_usuel like '%EDFNIVO'",
+        # French Alps domain
+        f"poste.lat_dg<={latmax} and poste.lat_dg>={latmin} and poste.lon_dg<={lonmax} and poste.lon_dg>={lonmin}",
+    ]
+    listvar = ["poste.num_poste", "poste.nom_usuel", "poste.lat_dg", "poste.lon_dg", "poste.alti",
+            "hist_reseau_poste.reseau_poste"]
+
+
+    if len(sys.argv) == 2:
+        period     = []
+        table      = 'POSTE'
+        listjoin   = ['HIST_RESEAU_POSTE ON HIST_RESEAU_POSTE.NUM_POSTE=POSTE.NUM_POSTE']
+        listorder  = ['poste.num_poste']
+        header     = ['num_poste', 'poste', 'lat', 'lon', 'alti', 'reseau_poste']
+        outputfile = 'liste_stations_ANTILOPE_{domain}.csv'
+
+    elif len(sys.argv) == 4:
+        datedeb = sys.argv[2]
+        datefin = sys.argv[3]
+        period = [datedeb, datefin]
+        table      = 'H'
+        listvar.extend(["h.dat", "h.rr1"])
+        listjoin       = ['POSTE ON POSTE.NUM_POSTE=H.NUM_POSTE ',
+            'HIST_RESEAU_POSTE ON HIST_RESEAU_POSTE.NUM_POSTE=H.NUM_POSTE']
+        listorder      = ['h.dat', 'h.num_poste']
+        header = ['dat', 'num_poste', 'poste', 'lat', 'lon', 'alti', 'reseau_poste', 'rr']
+        outputfile = f"obs_quotidiennes_RR_{domain}_{datedeb}_{datefin}.data"
+
+    else:
         usage()
-    datedeb = sys.argv[1]
-    datefin = sys.argv[2]
+
 
 # 1. RR horaires
-#-----------------
+# ---------------
     question1 = question(
-        listvar=["h.dat", "h.num_poste", "poste.nom_usuel", "poste.lat_dg", "poste.lon_dg", "poste.alti", "h.rr1", "hist_reseau_poste.reseau_poste"],
-        table='H',
-        listjoin=['POSTE ON H.NUM_POSTE=POSTE.NUM_POSTE ',
-            'HIST_RESEAU_POSTE ON HIST_RESEAU_POSTE.NUM_POSTE=POSTE.NUM_POSTE'],
-        #listjoin=['POSTE ON H.NUM_POSTE=POSTE.NUM_POSTE', 'HIST_TYPE_POSTE ON HIST_TYPE_POSTE.NUM_POSTE=POSTE.NUM_POSTE and hist_type_poste.dat=h.dat and hist_type_poste.type_poste<=3'],
-        # Pour la version "clim" :
-        #listjoin=['POSTE ON H.NUM_POSTE=POSTE.NUM_POSTE',
-        #   'HIST_RESEAU_POSTE ON HIST_RESEAU_POSTE.NUM_POSTE=POSTE.NUM_POSTE and hist_reseau_poste.datdeb<=h.dat and (hist_reseau_poste.datfin>=h.dat or hist_reseau_poste.datfin is null) and hist_reseau_poste.reseau_poste in (10,11,40,41,42,63,71)'],
-        #listjoin=['POSTE ON H.NUM_POSTE=POSTE.NUM_POSTE', 'HIST_TYPE_POSTE ON HIST_TYPE_POSTE.NUM_POSTE=POSTE.NUM_POSTE and hist_type_poste.dat=h.dat and hist_type_poste.type_poste<=4'],
-        listconditions = [
-            #"hist_reseau_poste.datdeb<=h.dat and (hist_reseau_poste.datfin>=h.dat or hist_reseau_poste.datfin is null) and hist_reseau_poste.reseau_poste in ('10','12','40','41','42','63','71')",
-            "hist_reseau_poste.datdeb<=h.dat and (hist_reseau_poste.datfin>=h.dat or hist_reseau_poste.datfin is null) and hist_reseau_poste.reseau_poste in ('10','12','63','71')",
-            "poste.lat_dg<=46.875 and poste.lat_dg>=42.75 and poste.lon_dg<=9. and poste.lon_dg>=4.5",
-        ],
-        #listconditions = ["hist_reseau_poste.datdeb<=h.dat and (hist_reseau_poste.datfin>=h.dat or hist_reseau_poste.datfin is null) and hist_reseau_poste.reseau_poste in ('10','11','40','41','42','63','71')"],
-        period=[datedeb, datefin],
-        listorder=['h.dat', 'h.num_poste'],
+        listvar        = listvar,
+        table          = table,
+        listjoin       = listjoin,
+        listconditions = listconditions,
+        period         = period,
+        listorder      = listorder,
     )
-    question1.run(outputfile="obs_quotidiennes_RR.data", header=['dat', 'num_poste', 'poste', 'lat', 'lon', 'alti', 'rr', 'reseau_poste'])
+    question1.run(outputfile=outputfile, header=header)
