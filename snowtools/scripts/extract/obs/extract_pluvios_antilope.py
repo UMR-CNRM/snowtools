7f48581ae99fe5e53ea55b34300a8c364eb5c0e2
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-10-14 09:34:55 +0200

    Compute precipitation accumulation over the extract period for ANTILOPE erro estimation method

--- snowtools/scripts/extract/obs/extract_pluvios_antilope.py
+++ snowtools/scripts/extract/obs/extract_pluvios_antilope.py
@@ -6,10 +6,11 @@
 # Script simplifié pour extraire les pluvios utilisés par la LE ANTILOPE.
 
 # Doc BDClim : http://dpnet.meteo.fr/DCLIM/doc/climsol.html
-# Doc réseaux postes : http://gedmf/share/page/document-details?nodeRef=workspace://SpacesStore/b31a01c0-d252-4e3e-ba86-9e1c25b5d0d5
+# Doc réseaux postes :
+# http://gedmf/share/page/document-details?nodeRef=workspace://SpacesStore/b31a01c0-d252-4e3e-ba86-9e1c25b5d0d5
 
 import sys
-
+import pandas as pd
 from snowtools.scripts.extract.obs.bdquery import question
 
 
@@ -39,13 +40,12 @@ if __name__ == "__main__":
     ]
     listvar = ["poste.num_poste", "poste.nom_usuel", "poste.lat_dg", "poste.lon_dg", "poste.alti",
             "hist_reseau_poste.reseau_poste"]
-
+    listorder  = ['poste.num_poste']
 
     if len(sys.argv) == 2:
         period     = []
         table      = 'POSTE'
         listjoin   = ['HIST_RESEAU_POSTE ON HIST_RESEAU_POSTE.NUM_POSTE=POSTE.NUM_POSTE']
-        listorder  = ['poste.num_poste']
         header     = ['num_poste', 'poste', 'lat', 'lon', 'alti', 'reseau_poste']
         outputfile = 'liste_stations_ANTILOPE_{domain}.csv'
 
@@ -57,8 +57,7 @@ if __name__ == "__main__":
         listvar.extend(["h.dat", "h.rr1"])
         listjoin       = ['POSTE ON POSTE.NUM_POSTE=H.NUM_POSTE ',
             'HIST_RESEAU_POSTE ON HIST_RESEAU_POSTE.NUM_POSTE=H.NUM_POSTE']
-        listorder      = ['h.dat', 'h.num_poste']
-        header = ['dat', 'num_poste', 'poste', 'lat', 'lon', 'alti', 'reseau_poste', 'rr']
+        header = ['num_poste', 'poste', 'lat', 'lon', 'alti', 'reseau_poste', 'date', 'rr']
         outputfile = f"obs_quotidiennes_RR_{domain}_{datedeb}_{datefin}.data"
 
     else:
@@ -76,3 +75,10 @@ if __name__ == "__main__":
         listorder      = listorder,
     )
     question1.run(outputfile=outputfile, header=header)
+
+    if len(sys.argv) == 4:
+        df = pd.read_csv(outputfile, sep=';')
+        out = df.groupby('num_poste').agg({'poste': 'min', 'lat': 'max', 'lon': 'max', 'alti': 'max',
+            'reseau_poste': 'max', 'rr': 'sum'})
+
+        out.to_csv(outputfile.replace('obs_quotidiennes', 'CUMUL'), sep=';')
