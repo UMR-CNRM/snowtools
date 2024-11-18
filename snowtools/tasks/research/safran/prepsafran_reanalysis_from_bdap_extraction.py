ebba31c166347b3405bb6d95cf90dfb90a5c2131
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2024-11-18 09:02:08 +0100

    Add task to deal with BDAP extractions more efficiently

---
+++ snowtools/tasks/research/safran/prepsafran_reanalysis_from_bdap_extraction.py
@@ -0,0 +1,182 @@
+# -*- coding:Utf-8 -*-
+
+
+__all__ = []
+
+import glob
+import os
+import tarfile
+
+from cen.layout.nodes import S2MTaskMixIn
+import footprints
+from vortex import toolbox
+from vortex.layout.nodes import Driver, Task
+
+logger = footprints.loggers.getLogger(__name__)
+
+
+def setup(t, **kw):
+    return Driver(
+        tag='bdap2guess',
+        ticket=t,
+        nodes=[
+            PrepSafran(tag='prepsaf', ticket=t, **kw),
+        ],
+        options=kw,
+    )
+
+
+class PrepSafran(Task, S2MTaskMixIn):
+
+    filter_execution_error = S2MTaskMixIn.s2moper_filter_execution_error
+
+    def refill(self):
+
+        pass
+
+    def process(self):
+        """Preparation of SAFRAN input files"""
+
+        def tb01_generic_hook1(t, rh):
+            sh = t.sh
+            tarname = sh.path.basename(rh.container.localpath())
+            if sh.is_tarfile(tarname):
+                sh.untar(tarname)
+            sh.rm(tarname)
+
+        t = self.ticket
+        self.missing_dates = list()
+
+        if 'early-fetch' in self.steps or 'fetch' in self.steps:
+
+            self.sh.title('Toolbox input tb_ebauche')
+            tb_ebauche = toolbox.input(
+                role            = 'Nam_ebauche',
+                source          = 'namelist_ebauche_[geometry]',
+                geometry        = self.conf.vconf,
+                genv            = self.conf.cycle,
+                kind            = 'namelist',
+                model           = self.conf.model,
+                local           = 'EBAUCHE_[vconf]',
+            )
+            print(t.prompt, 'tb_tb_ebauche =', tb_ebauche)
+            print()
+
+            interp = ''
+            datebegin = self.conf.datebegin
+            while datebegin < self.conf.dateend:
+
+                dateend = datebegin.replace(year=datebegin.year + 1)
+
+                self.sh.title('Toolbox input ARPEGE')
+                tb02 = toolbox.input(
+                    role           = 'Gridpoint',
+                    kind           = 'packedguess',
+                    local          = '[enddate:ymd6h]/ARPEGE_[begindate:ymd6h]_[enddate:ymd6h].tar',
+                    experiment     = 'reanalysis2020.3@vernaym',
+                    cutoff         = 'assimilation',
+                    block          = 'guess',
+                    nativefmt      = 'tar',
+                    namespace      = 'vortex.multi.fr',
+                    geometry       = self.conf.vconf,
+                    begindate      = datebegin.ymd6h,
+                    enddate        = dateend.ymd6h,
+                    model          = 'safran',
+                    namebuild      = 'flat@cen',
+                    date           = dateend.ymdh,
+                    vapp           = 'safran',
+                    vconf          = self.conf.vconf,
+                    fatal          = True,
+                    hook_autohook1 = (tb01_generic_hook1, ),
+                )
+                print(t.prompt, 'tb02 =', tb02)
+                print()
+
+                datebegin = dateend
+
+            self.sh.title('Toolbox input tb03 = PRE-TRAITEMENT FORCAGE script')
+            tb03 = script = toolbox.input(
+                role        = 'pretraitement',
+                local       = 'makeP.py',
+                genv        = self.conf.cycle,
+                kind        = 's2m_filtering_grib',
+                language    = 'python',
+                rawopts     = '{0:s} -a -d {1:s} -f '.format(interp, self.conf.vconf) +
+                              ' '.join(glob.glob('ARPEGE*.grib')),
+            )
+            print(t.prompt, 'tb03 =', tb03)
+            print()
+
+        if 'compute' in self.steps:
+
+            self.sh.title('Toolbox algo tb04')
+            expresso = toolbox.algo(
+                vconf          = self.conf.vconf,
+                engine         = 'exec',
+                kind           = 'guess',
+                interpreter    = 'current',
+                # Need to extend pythonpath to be independant of the user environment
+                # The vortex-build environment already set up the pythonpath (see jobassistant plugin) but the script is
+                # eventually launched in a 'user-defined' environment
+                extendpypath   = [self.sh.path.join('/'.join(self.conf.iniconf.split('/')[:-2]), d)
+                                  for d in ['vortex/src', 'vortex/site', 'epygram',
+                                            'epygram/site', 'epygram/eccodes_python']],
+                ntasks         = self.conf.ntasks,
+                terms          = footprints.util.rangex(self.conf.ana_terms),
+            )
+            print(t.prompt, 'tb04 =', expresso)
+            print()
+
+#             self.sh.title('Toolbox algo tb04')
+#             expresso = toolbox.algo(
+#                 vconf          = self.conf.vconf,
+#                 engine         = 'exec',
+#                 kind           = 'guess',
+#                 #terms          = footprints.util.rangex(self.conf.prv_terms),
+#                 #members        = footprints.util.rangex(1, 40, 1),
+#                 interpreter    = script[0].resource.language,
+#                 ntasks         = self.conf.ntasks,
+#                 # members        = footprints.util.rangex(self.conf.members)
+#             )
+#             print t.prompt, 'tb04 =', expresso
+#             print
+
+            self.component_runner(expresso, script, fortran = False)
+
+        if 'backup' in self.steps or 'late-backup' in self.steps:
+
+            # WARNING : The following only works for a 1-year execution
+            season = self.conf.datebegin.nivologyseason
+            tarname = 'p{0:s}.tar'.format(season)
+            # thisdir = os.getcwd()
+            with tarfile.open(tarname, mode='w') as tarfic:
+                for f in glob.glob('*/P????????'):
+                    # oldname = os.path.basename(f).split('_')[0]
+                    # date = Date.strptime(oldname[1:], '%y%m%d%H') + Period(hours=6)
+                    # arcname = 'P{0:s}'.format(date.yymdh)
+                    arcname = os.path.basename(f)
+                    tarfic.add(f, arcname=arcname)
+
+            self.sh.title('Toolbox output tb05')
+            tb05 = toolbox.output(
+                role           = 'Ebauche',
+                local          = tarname,
+                remote         = '/home/vernaym/s2m/[geometry]/{0:s}/{1:s}'.format(self.conf.guess_block, tarname),
+                hostname       = 'hendrix.meteo.fr',
+                unknownflow    = True,
+                username       = 'vernaym',
+                tube           = 'ftp',
+                geometry       = self.conf.vconf,
+                cumul          = self.conf.cumul,
+                cutoff         = 'assimilation',
+                nativefmt      = 'ascii',
+                kind           = 'guess',
+                model          = 'safran',
+                date           = self.conf.dateend.ymd6h,
+            ),
+            print(t.prompt, 'tb05 =', tb05)
+            print()
+
+            from vortex.tools.systems import ExecutionError
+            raise ExecutionError('')
+            pass
