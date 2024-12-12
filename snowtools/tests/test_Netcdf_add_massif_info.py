aace704bdcc91812f75dc081e371374dc788e743
Leo VIALLON-GALINIER <leo.viallon@meteo.fr>
2024-12-12 18:21:23 +0100

    Tests: Do not use path of executable but access by module

--- snowtools/tests/test_Netcdf_add_massif_info.py
+++ snowtools/tests/test_Netcdf_add_massif_info.py
@@ -9,7 +9,6 @@ import subprocess
 
 from netCDF4 import Dataset
 
-from snowtools.DATA import SNOWTOOLS_DIR
 from snowtools.DATA import TESTBASE_DIR
 from snowtools.tests.tempfolder import TestWithTempFolder
 
@@ -20,9 +19,9 @@ from snowtools.tests.tempfolder import TestWithTempFolder
 class TestScript(TestWithTempFolder):
     def test_script(self):
         path_forcing = os.path.join(TESTBASE_DIR, "FORCING", 'FORCING_test_2d.nc')
-        path_script = os.path.join(SNOWTOOLS_DIR, 'interpolation/Netcdf_add_massif_info.py')
         path_out = os.path.join(self.diroutput, 'test.nc')
-        subprocess.run([sys.executable, path_script, path_forcing, "-o", path_out], env=os.environ.copy())
+        subprocess.run([sys.executable, '-m', 'snowtools.interpolation.Netcdf_add_massif_info',
+                        path_forcing, "-o", path_out], env=os.environ.copy())
         with Dataset(path_out, 'r', format='NETCDF4') as file_input:
             self.assertTrue((file_input.variables['massif_num'][Ellipsis] == 69).all())
 
