9d4f774ea79b7047a9b1c8fb8f9e4efb5a0f6750
Matthieu Lafaysse <matthieu.lafaysse@meteo.fr>
2025-08-29 17:36:52 +0200

    remove automatic import not suitable with default install

--- snowtools/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from . import utils  # noqa
-
-# disble if unavailable
-from bronx.syntax.externalcode import ExternalCodeImportChecker
-
-vortex_checker = ExternalCodeImportChecker('vortex')
-with vortex_checker:
-    from . import algo  # noqa
