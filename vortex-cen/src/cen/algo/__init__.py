eca0bb42083d1304e477d88b0f0be05dcb417e52
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2026-02-18 11:53:04 +0100

    Add algo component to create SAFRAN-ready reconstructed observations

--- vortex-cen/src/cen/algo/__init__.py
+++ vortex-cen/src/cen/algo/__init__.py
@@ -2,7 +2,13 @@
 Specific CEN AlgoComponents.
 """
 
-from . import assim, deterministic, ensemble, scores, postprocessing, forcing
+from . import assim
+from . import deterministic
+from . import  ensemble
+from . import scores
+from . import postprocessing
+from . import forcing
+from . import obs
 
 #: No automatic export
 __all__ = []
