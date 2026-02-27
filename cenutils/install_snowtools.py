1abf5275d570bdf63ab62a52970fb94ba7837376
Nicola Imperatore <nicola.imperatore@meteo.fr>
2026-02-27 15:03:29 +0100

    fix install issue on Belenos for Nicola, add error message while installing snowtools, update README_install

--- cenutils/install_snowtools.py
+++ cenutils/install_snowtools.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import shutil
-import glob
 import argparse
+import glob
+import os
+import shutil
 import subprocess
+import sys
 
 # TODO : Use a proper API of the venv package
 # see https://docs.python.org/3/library/venv.html
@@ -14,19 +14,31 @@ import subprocess
 description = "Snowtools installation script for MF developpers"
 parser = argparse.ArgumentParser(description=description)
 
-parser.add_argument('-e', '--editable', action='store_true',
-                    help="Install editable version for onging developments")
+parser.add_argument("-e", "--editable", action="store_true", help="Install editable version for onging developments")
 
-parser.add_argument('-v', '--venv', type=str, required=False, default=None,
-                    help="Path (relative or absolute) to the virtual environment to be created."
-                         "If this script is already called from a virtual environment,"
-                         "this argument is ignored.")
+parser.add_argument(
+    "-v",
+    "--venv",
+    type=str,
+    required=False,
+    default=None,
+    help="Path (relative or absolute) to the virtual environment to be created."
+    "If this script is already called from a virtual environment,"
+    "this argument is ignored.",
+)
 
-parser.add_argument('-o', '--optional', action='store', choices=['plot', 'sql', 'all'], nargs='+', default=None,
-                    help="Install optional dependencies (this option is ignored on MF's HPC):\n" +
-                         "* 'plot' install graphical tools\n" +
-                         "* 'sql' install sql extraction tools\n" +
-                         "* 'all' install all optionnal dependencies")
+parser.add_argument(
+    "-o",
+    "--optional",
+    action="store",
+    choices=["plot", "sql", "all"],
+    nargs="+",
+    default=None,
+    help="Install optional dependencies (this option is ignored on MF's HPC):\n"
+    + "* 'plot' install graphical tools\n"
+    + "* 'sql' install sql extraction tools\n"
+    + "* 'all' install all optionnal dependencies",
+)
 
 args = parser.parse_args()
 
@@ -34,10 +46,10 @@ args = parser.parse_args()
 snowtools_dir = os.path.dirname(os.path.dirname(__file__))
 
 # Retrieve server name to activate server-specific installation steps
-HOSTNAME = os.getenv('HOSTNAME', '')
+HOSTNAME = os.getenv("HOSTNAME", "")
 
 # Check for packages installed locally to issue a warning.
-if glob.glob(os.path.join(os.environ['HOME'], '.local', 'lib', 'python*')):
+if glob.glob(os.path.join(os.environ["HOME"], ".local", "lib", "python*")):
     print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     print("WARNING: It looks like you have locally installed python packages.")
     print("You should re-install these packages in a dedicated virtual environment and remove them with:")
@@ -46,9 +58,11 @@ if glob.glob(os.path.join(os.environ['HOME'], '.local', 'lib', 'python*')):
 
 # Virtual environment
 # -------------------
-outstr = '=====================================================================\n' \
-         '                     INSTALLATION INFORMATION                        \n' \
-         '=====================================================================\n'
+outstr_header = (
+    "=====================================================================\n"
+    "                     INSTALLATION INFORMATION                        \n"
+    "=====================================================================\n"
+)
 
 if sys.base_prefix == sys.prefix:
     # The script was NOT called from within a virtual environment
@@ -61,38 +75,46 @@ if sys.base_prefix == sys.prefix:
         # Security: An editable install in a virtual environment within the snowtools repository leads meson to crash
         # See related comment above
         if args.editable and snowtools_dir in venv:
-            raise Exception("For an editable install, the virtual environment must not be created "
-                            "in the snowtools root directory.\n"
-                            "Please choose a different path for the creation of your virtual environment")
+            raise Exception(
+                "For an editable install, the virtual environment must not be created "
+                "in the snowtools root directory.\n"
+                "Please choose a different path for the creation of your virtual environment"
+            )
 
-        if not os.path.isfile(os.path.join(venv, 'bin', 'pip')):
+        if not os.path.isfile(os.path.join(venv, "bin", "pip")):
             # Create the virtual environment if it does not exist already
             from venv import create
+
             create(venv, with_pip=True)
             # TODO : ajouter un message pour dire comment activer l'environnement virtuel créé ?
-            outstr = outstr + "Snowtools has been installed in a new virtual environment.\n" \
-                "To activate it, run :\n" \
+            outstr = (
+                outstr_header + "Snowtools has been installed in a new virtual environment.\n"
+                "To activate it, run :\n"
                 f"source {venv}/bin/activate"
+            )
         else:
             # TODO : quel comportement dans le cas où l'environnement virtuel existe déjà ?
             # comportement actuel : on tente l'installation dans cet environnement
-            outstr = outstr + "Snowtools has been installed in an existing virtual environment.\n" \
-                "To activate it, run :\n" \
+            outstr = (
+                outstr_header + "Snowtools has been installed in an existing virtual environment.\n"
+                "To activate it, run :\n"
                 f"source {venv}/bin/activate"
+            )
 
         # Activate the virtual environment
-        os.environ['PATH'] = ':'.join([os.path.join(venv, 'bin'), os.environ['PATH']])
-        pip = os.path.join(venv, 'bin', 'pip')
+        os.environ["PATH"] = ":".join([os.path.join(venv, "bin"), os.environ["PATH"]])
+        pip = os.path.join(venv, "bin", "pip")
         sys.prefix = venv
         sys.exec_prefix = venv
 
     else:
-
-        raise SystemError('It looks like you are not in a virtual environment.\n'
-                'Please activate a virtual environment or create one with the -v/--venv argument.')
+        raise SystemError(
+            "It looks like you are not in a virtual environment.\n"
+            "Please activate a virtual environment or create one with the -v/--venv argument."
+        )
 else:
     # The script was called from within a virtual environment
-    outstr = outstr + "Snowtools has been installed in the current virtual environment."
+    outstr = outstr_header + "Snowtools has been installed in the current virtual environment."
 
     # Security : an editable install in a virtual environment within the snowtools repository leads meson to crash:
     # ```
@@ -101,44 +123,51 @@ else:
     #      directories that are part of your project.
     # ```
     if args.editable and snowtools_dir in sys.executable:
-        raise Exception("It looks like the current virtual environment is at the snowtools root directory.\n"
-                        "An editable install is not possible in this case.\n"
-                        "Please create your virtual environement elsewhere or install snowtools as non-editable")
+        raise Exception(
+            "It looks like the current virtual environment is at the snowtools root directory.\n"
+            "An editable install is not possible in this case.\n"
+            "Please create your virtual environement elsewhere or install snowtools as non-editable"
+        )
 
     venv = sys.prefix
-    pip = 'pip'
+    pip = "pip"
 
-if args.optional is None or 'hpc' in HOSTNAME:
+if args.optional is None or "hpc" in HOSTNAME:
     # Optional dependencies are unavailable on MF HPC (that is the reason they are optional)
     print("The '-o' argument will be ignored because optional dependencies are not available on MF HPC")
-    optional = ''
+    optional = ""
 else:
-    optional = '[' + ','.join(args.optional) + ']'
+    optional = "[" + ",".join(args.optional) + "]"
 
-if '-sidev' in HOSTNAME:
+if "-sidev" in HOSTNAME:
     # On SOPRANO servers, the following pip arguments are required to enable the connexion to PyPI
-    pip_options = ['--trusted-host', 'pypi.org', '--trusted-host', 'pypi.python.org', '--trusted-host',
-            'files.pythonhosted.org']
+    pip_options = [
+        "--trusted-host",
+        "pypi.org",
+        "--trusted-host",
+        "pypi.python.org",
+        "--trusted-host",
+        "files.pythonhosted.org",
+    ]
 else:
     pip_options = list()
 
 # Ensure to use the latest available pip version
 print("Running command:")
 print(f"{pip} install --upgrade pip")
-subprocess.run([pip, 'install'] + pip_options + ['--upgrade', 'pip'])
+subprocess.run([pip, "install"] + pip_options + ["--upgrade", "pip"])
 
 # Snowtools installation
 # ----------------------
 
 os.chdir(snowtools_dir)
 # Security : an existing "build" directory from a former installation may cause trouble
-shutil.rmtree('build', ignore_errors=True)
-shutil.rmtree('.mesonpy*', ignore_errors=True)
+shutil.rmtree("build", ignore_errors=True)
+shutil.rmtree(".mesonpy*", ignore_errors=True)
 
 if args.editable:
-
     if sys.version_info < (3, 10, 1):
-        raise SystemError('Editable install is not possible with python versions lower than 3.10')
+        raise SystemError("Editable install is not possible with python versions lower than 3.10")
 
     # Snowtools contains a compiled extension module written in Fortran.
     # In order to render compiled extension modules editable similarly to ordinary python code,
@@ -151,25 +180,30 @@ if args.editable:
     # https://mesonbuild.com/meson-python/how-to-guides/editable-installs.html
     print("Running command:")
     print(f"{pip} install {' '.join(pip_options)} numpy>=1.21.6 meson-python ninja")
-    subprocess.run([pip, 'install'] + pip_options + ['numpy>=1.21.6', 'meson-python', 'ninja'])
+    subprocess.run([pip, "install"] + pip_options + ["numpy>=1.21.6", "meson-python", "ninja"])
 
     # 'no-build-isolation' is required for an editable install
-    pip_options.extend(['--no-build-isolation', '-e'])
+    pip_options.extend(["--no-build-isolation", "-e"])
 
 
 # Install snowtools snowtools
 # pip install [--no-build-isolation -e] .
 print("Running command:")
-print(f"{pip} install {' '.join(pip_options)} .{optional}")
-subprocess.run([pip, 'install'] + pip_options + [f'.{optional}'])
+cmd_install = f"{pip} install {' '.join(pip_options)} .{optional}"
+print(cmd_install)
+
+out = subprocess.run([pip, "install"] + pip_options + [f".{optional}"])
+if out.returncode == 1:
+    outstr = outstr_header + f"An error has occurred during the installation with {cmd_install}"
+
 
 # Write latest snowtools commit number into the virtual environment to keep a track of what has just been installed
-if os.path.isdir('.git'):
-    commit = subprocess.check_output('git show --pretty=format:"%H" --no-patch', shell=True, encoding='utf-8')
-    with open(os.path.join(venv, '.snowtools_info'), 'a') as f:
+if os.path.isdir(".git"):
+    commit = subprocess.check_output('git show --pretty=format:"%H" --no-patch', shell=True, encoding="utf-8")
+    with open(os.path.join(venv, ".snowtools_info"), "a") as f:
         f.write(commit)
-elif os.path.exists('.git_info'):
-    shutil.copyfile('.git_info', os.path.join(venv, '.snowtools_info'))
+elif os.path.exists(".git_info"):
+    shutil.copyfile(".git_info", os.path.join(venv, ".snowtools_info"))
 
 print(outstr)
 print()
