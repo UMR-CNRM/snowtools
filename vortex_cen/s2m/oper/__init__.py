35ead7165a6958af493b0b7f8d03c5a51e43e5fa
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2026-06-12 14:06:35 +0200

    Update S2M configurations and documentation

--- vortex_cen/s2m/oper/__init__.py
+++ vortex_cen/s2m/oper/__init__.py
@@ -4,4 +4,44 @@ The s2m "oper" configuration
 ============================
 
 Operational SAFRAN-SURFEX/Crocus-MEPRA (S2M) model chain.
+
+Workflow :
+----------
+
+1. SAFRAN inputs
+
+    * prepsafran_analyse : Generation of SAFRAN guess files from NWP analyses
+
+    * prepsafran_prevision : Generation of SAFRAN guess files from NWP forecasts
+
+    * prepsafran_reanlayse : Generation of archives of SAFRAN guess files from the previous 01/08
+
+    * [refill_guess_safran] : Offline generation of an archive of SAFRAN guess files to restart the real-time chain
+
+
+2. SAFRAN
+
+    * safran_analyse_ensemble : Ensemble (runs 3H, 6H, 9H) or detterministic (run 12H) SAFRAN analyses
+    (generation of "flat" FORCING files)
+
+    * safran_analysei_prevision : Ensemble SAFRAN forecasts (generation of "flat" FORCING files)
+
+3. SURFEX
+
+    * ensemble_surfex_tasks_analysis : Update raw SAFRAN forcing files (add slopes and solar masks) + ensemble
+    SURFEX/Crocus snowpack simulations
+
+    * ensemble_surfex_tasks_forecast : Update raw SAFRAN forcing files (add slopes and solar masks) + ensemble
+    SURFEX/Crocus snowpack simulations
+
+    * monthly_surfex_reanalysis : Deterministic SURFEX/Crocus snowpack simulation from the previous 01/08
+
+    * monthly_surfex_reanalysis_sytron : Deterministic SURFEX/Crocus snowpack simulation from the previous 01/08 with
+    the SYTRON snow drift module activated
+
+4. Post-processing
+
+    * hydro_task : Hydrological post-processing
+
+    * postprocess_forecast : Post-processing for the 4 seasons bulletin
 """
--- vortex_cen/s2m/reanalysis/__init__.py
+++ vortex_cen/s2m/reanalysis/__init__.py
@@ -5,4 +5,38 @@ The s2m "reanalysis" configuration
 
 Production of the reference SAFRAN-SURFEX/Crocus-Mepra (S2M) reanalysis dataset
 
+Workflow :
+----------
+
+1. SAFRAN inputs
+
+    * obs_safran : Generation of SAFRAN-compatible reconstructed hourly surface observation time series
+
+    * prepsafran : Generation of SAFRAN guess files
+
+
+2. SURFEX inputs
+
+    * safran : Generation of "raw" SAFRAN meteorological forcing files ("flat" geometry and 1 "postes" FORCING file
+    per domain)
+
+    * [init_TG] : Generation of an initial ground temperature]
+
+    * [prep] : Generation of initial snowpack state
+
+    * [pgd] : Generation of ground physiography
+
+3. FORCING modification
+
+    * add_slopes : Add slopes and aspects to the raw "flat" SAFRAN meteorological forcing files
+
+    * concatenation_postes : Concatenate "postes" forcing files from all domains into a single FORCING file and add
+    solar masks
+
+4. SURFEX
+
+    * surfex_massifs : Generation of SURFEX/Crocus snowpack simulations on an "allslopes" geometry
+
+    * surfex_postes : Generation of SURFEX/Crocus snowpack simulations on the "postes" geometry
+
 """
