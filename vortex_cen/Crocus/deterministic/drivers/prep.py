9a3622db7a019a7ec04e8f1fafa089777d238404
VERNAY MATTHIEU <matthieu.vernay@meteo.fr>
2026-07-01 17:38:21 +0200

    DBUG prep generation task
    Include namelist preprocessing (mandatory) and init_TG generation
    to avoid having to launch the "init_TG" job beforehand

--- vortex_cen/Crocus/deterministic/drivers/prep.py
+++ vortex_cen/Crocus/deterministic/drivers/prep.py
@@ -5,6 +5,8 @@ This "prep" driver allows to generate a PREP.nc file (initial conditions) from a
 """
 
 from mkjob.nodes import Driver
+from vortex_cen.tasks.surfex.pre_process import Preprocess_Uenv_Namelist
+from vortex_cen.tasks.surfex.init_clim_ground_temperature import InitClimGroundTemperature
 from vortex_cen.tasks.surfex.prep import _Prep_Construct
 
 
@@ -13,6 +15,8 @@ def setup(t, **kw):
         tag='prep',
         ticket=t,
         nodes=[
+            InitClimGroundTemperature(tag='inittg', ticket=t, **kw),
+            Preprocess_Uenv_Namelist(tag='preprocess', ticket=t, **kw),
             MakePrep(tag='makeprep', ticket=t, **kw),
         ],
         options=kw,
@@ -21,11 +25,9 @@ def setup(t, **kw):
 
 class MakePrep(_Prep_Construct):
 
-    def get_init_TG(self):
-        self.get_init_TG_from_uenv()
-
-    def get_namelist(self):
-        self.get_namelist_from_uenv()
-
     def get_prep_executable(self):
         self.get_prep_exe_from_uenv()
+
+    def get_namelist(self):
+        # This task must be launched after a namelist pre-process task
+        self.get_namelist_from_cache()
