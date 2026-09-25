UMR-CNRM/snowtools

commit 41e96bddfd278c71972c59eb533ea67a76969376

some renaming and moveing.
In particular everything that used to be in plot.pearps2m.

snowtools/plots/stratiprofile/proplotter_gui.py
@@ -631,7 +631,7 @@ class ProPlotterControllerEscroc(ProPlotterControllerSlider):
         Update the date scaler after opening a file.
         """
         if self.master.fileobj is not None:
-            self.master.choices.params_w.scale_date.config(
+            self.master.choices.params_w.scale_date.Config(
                 from_=0, to=(len(self.master.fileobj.get_time()) - 1),
                 state='normal', showvalue=0, variable=tk.IntVar,
                 command=lambda _: (self.master.choices.params_w.update_slice_date(), self.update_slice_date()))
@@ -715,7 +715,7 @@ class ProPlotterControllerMultiple(ProPlotterControllerSlider):
         Update the date scaler after opening a file.
         """
         if self.master.fileobj is not None:
-            self.master.choices.params_w.scale_date.config(
+            self.master.choices.params_w.scale_date.Config(
                 from_=0, to=(len(self.master.fileobj.get_time()) - 1),
                 state='normal', showvalue=0, variable=tk.IntVar,
                 command=lambda _: (self.master.choices.params_w.update_slice_date(), self.update_slice_date()))

snowtools/data/ensemble_classes.py
@@ -6,14 +6,7 @@ Created on 6 déc. 2018
 
 @author: lafaysse
 
-usage: python postprocess.py [-b YYYYMMDD] [-e YYYYMMDD] [-o diroutput]
-
-    #) extracts operational simulation results
-    #) Plots maps for the Alps, the Pyrenees the Corse, Vosges, Massif Central, Jura
-    #) Creates spaghetti plots for all massifs and stations
 """
-
-import argparse
 import os
 import datetime
 
@@ -21,14 +14,9 @@ import numpy as np
 import pandas as pd
 from scipy.stats import gamma
 
-#from snowtools.plots.pearps2m.postprocess_plot import Config
-#from snowtools.plots.pearps2m.postprocess_plot import main
-#from snowtools.plots.pearps2m.postprocess_plot import pp_plots
 from snowtools.utils.prosimu import prosimu
 from snowtools.utils.infomassifs import infomassifs
-from snowtools.DATA import LUSTRE_NOSAVE_USER_DIR, SNOWTOOLS_DATA
-
-from bronx.stdtypes.date import today
+from snowtools.DATA import SNOWTOOLS_DATA
 
 
 def build_filename(massif, alti):
@@ -921,28 +909,3 @@ class EnsembleHydro(EnsemblePostproc):
     def standardvars(self):
         return ['time', 'basin']
 
-
-if __name__ == "__main__":
-    USAGE = "usage: python postprocess.py [-b YYYYMMDD] [-e YYYYMMDD] [-o diroutput]"
-
-    PARSER = argparse.ArgumentParser(description="Postprocess new snow heights: "
-                                                 "1) extracts operational simulation results,"
-                                                 "2) Plots maps for the Alps, the Pyrenees the Corse,"
-                                                 "3) Creates spaghetti plots "
-                                                 "for all massifs and stations")
-
-    PARSER.add_argument("-b", action="store", type=str, dest="datebegin", default=today().ymd,
-                        help="First year of extraction")
-    PARSER.add_argument("-e", action="store", type=str, dest="dateend", default=today().ymd,
-                        help="Last year of extraction")
-    PARSER.add_argument("-o", action="store", type=str, dest="diroutput",
-                        default=os.path.join(LUSTRE_NOSAVE_USER_DIR, "PEARPS2M"),
-                        help="Output directory")
-    PARSER.add_argument("--dev", action="store_true", dest="dev", default=False)
-    PARSER.add_argument("--reforecast", action="store_true", dest="reforecast", default=False)
-    PARSER.add_argument("--dble", action="store_true", dest="dble", default=False)
-    OPTIONS = PARSER.parse_args()  # @UnusedVariable
-    c = Config(OPTIONS)
-    main(c)
-    if OPTIONS.dev:
-        pp_plots(c)

snowtools/data/obs.py
@@ -1,38 +1,0 @@
-"""
-S2M executions flow resources.
-"""
-
-from bronx.fancies import loggers
-
-from cen.data.flow import SurfaceIO
-
-
-#: No automatic export
-__all__ = []
-
-logger = loggers.getLogger(__name__)
-
-
-class SurfaceObservations(SurfaceIO):
-    """
-    Class for all kind of meteorological observation files.
-    """
-    _footprint = [
-        dict(
-            info = 'Meteorological surface observations',
-            attr = dict(
-                kind = dict(
-                    values = ['SurfaceObservation'],
-                ),
-                model = dict(
-                    optional = True,  # The 'model' footprint of FORCING files is mostly unknown/undefined (and unused)
-                    deafult  = 'unknown',
-                    values   = ['safran'],
-                ),
-            )
-        )
-    ]
-
-    @property
-    def realkind(self):
-        return self.kind

snowtools/plots/pearps2m/oper_diags_plot.py
@@ -15,9 +15,9 @@ import matplotlib.style
 from snowtools.utils.prosimu import prosimu
 from snowtools.utils.dates import check_and_convert_date, pretty_date
 from snowtools.plots.temporal.chrono import spaghettis_with_det, spaghettis
-#from snowtools.plots.pearps2m.postprocess import _EnsembleMassif
-#from snowtools.plots.pearps2m.postprocess import EnsembleFlatMassif, EnsembleNorthSouthMassif
-#from snowtools.plots.pearps2m.postprocess import EnsembleStation, EnsembleDiags
+from snowtools.data.ensemble_classes import _EnsembleMassif
+from snowtools.data.ensemble_classes import EnsembleFlatMassif, EnsembleNorthSouthMassif
+from snowtools.data.ensemble_classes import EnsembleStation, EnsembleDiags
 
 from bronx.syntax.externalcode import ExternalCodeImportChecker
 

snowtools/scripts/observations/save_reconstructed_observations_with_vortex.py
@@ -2,14 +2,14 @@
 import argparse
 import os
 
-from vortex import toolbox
+import vortex
 
 from snowtools.utils.dates import get_list_dates_files, get_dic_dateend
 from bronx.stdtypes.date import Date
 
-from snowtools.data import obs  # noqa
+from vortex_cen.data import obs  # noqa
 
-toolbox.active_now = True
+vortex.toolbox.active_now = True
 
 description = "Archive reconstructed observation dataset for SAFRAN reanalysis"
 parser = argparse.ArgumentParser(description=description)
@@ -35,7 +35,7 @@ else:
     user = os.environ['USER']
     xpid = args.xpid + '@' + user
 
-toolbox.output(
+vortex.output(
     kind       = 'SurfaceObservation',
     nativefmt  = 'netcdf',
     model      = 'safran',

snowtools/scripts/post_processing/oper_graph_prod.py
@@ -1,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+usage: python postprocess.py [-b YYYYMMDD] [-e YYYYMMDD] [-o diroutput]
+
+    #) extracts operational simulation results
+    #) Plots maps for the Alps, the Pyrenees the Corse, Vosges, Massif Central, Jura
+    #) Creates spaghetti plots for all massifs and stations
+
+"""
+import argparse
+import os
+from bronx.stdtypes.date import today
+from snowtools.DATA import LUSTRE_NOSAVE_USER_DIR
+from snowtools.plots.pearps2m.oper_diags_plot import Config
+from snowtools.plots.pearps2m.oper_diags_plot import main
+from snowtools.plots.pearps2m.oper_diags_plot import pp_plots
+
+if __name__ == "__main__":
+    USAGE = "usage: python postprocess.py [-b YYYYMMDD] [-e YYYYMMDD] [-o diroutput]"
+
+    PARSER = argparse.ArgumentParser(description="Postprocess new snow heights: "
+                                                 "1) extracts operational simulation results,"
+                                                 "2) Plots maps for the Alps, the Pyrenees the Corse,"
+                                                 "3) Creates spaghetti plots "
+                                                 "for all massifs and stations")
+
+    PARSER.add_argument("-b", action="store", type=str, dest="datebegin", default=today().ymd,
+                        help="First year of extraction")
+    PARSER.add_argument("-e", action="store", type=str, dest="dateend", default=today().ymd,
+                        help="Last year of extraction")
+    PARSER.add_argument("-o", action="store", type=str, dest="diroutput",
+                        default=os.path.join(LUSTRE_NOSAVE_USER_DIR, "PEARPS2M"),
+                        help="Output directory")
+    PARSER.add_argument("--dev", action="store_true", dest="dev", default=False)
+    PARSER.add_argument("--reforecast", action="store_true", dest="reforecast", default=False)
+    PARSER.add_argument("--dble", action="store_true", dest="dble", default=False)
+    OPTIONS = PARSER.parse_args()  # @UnusedVariable
+    c = Config(OPTIONS)
+    main(c)
+    if OPTIONS.dev:
+        pp_plots(c)

snowtools/scripts/post_processing/secure_graph_prod.py
@@ -3,10 +3,9 @@
 import os
 import argparse
 
-from snowtools.plots.pearps2m.postprocess_plot import config
+from snowtools.plots.pearps2m.oper_diags_plot import Config
 from bronx.stdtypes.date import today
 from snowtools.DATA import LUSTRE_NOSAVE_USER_DIR
-from snowtools.plots.pearps2m.postprocess import Config
 
 usage = "usage: python secure_graph_prod.py [-b YYYYMMDD] [-e YYYYMMDD] [-o diroutput]"
 

snowtools/tasks/oper/get_oper_files.py
@@ -8,10 +8,9 @@ Created on 3 aug. 2018
 import sys
 # from optparse import OptionParser
 import argparse
+import vortex
 from collections import defaultdict
-
-from cen.layout.nodes import S2MTaskMixIn
-from vortex import toolbox
+from vortex_cen.tasks.oper_research_mixin import CENTaskMixIn
 from bronx.stdtypes.date import Date, daterange, tomorrow, today
 
 from snowtools.utils.dates import check_and_convert_date
@@ -20,7 +19,7 @@ import footprints
 
 
 
-class configdev(object):
+class Configdev(object):
     rundate = Date(2018, 10, 26, 3)    # Run date can be at 3TU, 6TU, 9TU
     previ = False  # False for analysis, True for forecast
     xpid = "OPER@lafaysse"  # To be changed with IGA account when operational
@@ -30,7 +29,7 @@ class configdev(object):
     firstday = False
 
 
-class config(object):
+class Config(object):
     rundate = Date(2018, 10, 26, 3)    # Run date can be at 3TU, 6TU, 9TU
     previ = False  # False for analysis, True for forecast
     xpid = "oper"
@@ -41,7 +40,7 @@ class config(object):
 
 
 
-class _configcommand(object):
+class _Configcommand(object):
 
     def __init__(self, options):
 
@@ -63,15 +62,15 @@ class _configcommand(object):
         self.pp_quantiles = options.pp_quantiles
 
 
-class configcommand(_configcommand, config):
+class Configcommand(_Configcommand, Config):
     pass
 
 
-class configcommanddev(_configcommand, configdev):
+class Configcommanddev(_Configcommand, Configdev):
     pass
 
 
-class S2MExtractor(S2MTaskMixIn):
+class S2MExtractor(CENTaskMixIn):
     """
     Class to extract S2M results
     """
@@ -81,7 +80,7 @@ class S2MExtractor(S2MTaskMixIn):
 
         :param conf: configuration information for vortex toolboxes
         """
-        toolbox.active_now = True
+        vortex.toolbox.active_now = True
         self.conf = conf
         self.datebegin, self.dateend = self.get_period()
         if not hasattr(self.conf, 'firstday'):
@@ -109,7 +108,7 @@ class S2MExtractor(S2MTaskMixIn):
 
     def get_meteo(self):
 
-        tb01 = toolbox.input(
+        tb01 = vortex.input(
             role           = 'Forcing',
             vapp           = 's2m',
             vconf          = '[geometry::area]',
@@ -136,7 +135,7 @@ class S2MExtractor(S2MTaskMixIn):
                     list_geometry = self.conf.alternate_list_geometry[a]
                 else:
                     list_geometry = self.conf.list_geometry
-                tb01.extend(toolbox.input(
+                tb01.extend(vortex.input(
                     alternate      = 'Forcing',
                     vapp           = 's2m',
                     vconf          = '[geometry::iganame]',
@@ -160,8 +159,8 @@ class S2MExtractor(S2MTaskMixIn):
         return self.get_std(tb01)
 
     def get_snow(self):
-        import cen
-        tb02 = toolbox.input(
+        import vortex_cen
+        tb02 = vortex.input(
             role           = 'pro',
             vapp           = 's2m',
             vconf          = '[geometry::iganame]',
@@ -190,7 +189,7 @@ class S2MExtractor(S2MTaskMixIn):
                 else:
                     list_geometry = self.conf.list_geometry
 
-                tb02.extend(toolbox.input(
+                tb02.extend(vortex.input(
                     alternate      = 'pro',
                     vapp           = 's2m',
                     vconf          = '[geometry::area]',
@@ -245,7 +244,7 @@ class FutureS2MExtractor(S2MExtractor):
 
     def get_meteo(self):
 
-        tb01 = toolbox.input(
+        tb01 = vortex.input(
             role           = 'Forcing',
             vapp           = 's2m',
             vconf          = '[geometry::area]',
@@ -272,7 +271,7 @@ class FutureS2MExtractor(S2MExtractor):
                     list_geometry = self.conf.alternate_list_geometry[a]
                 else:
                     list_geometry = self.conf.list_geometry
-                tb01.extend(toolbox.input(
+                tb01.extend(vortex.input(
                     alternate      = 'Forcing',
                     vapp           = 's2m',
                     vconf          = '[geometry::area]',
@@ -296,8 +295,8 @@ class FutureS2MExtractor(S2MExtractor):
         return self.get_std(tb01)
 
     def get_snow(self):
-        import cen
-        tb02 = toolbox.input(
+        import vortex_cen
+        tb02 = vortex.input(
             role           = 'pro',
             vapp           = 's2m',
             vconf          = '[geometry::area]',
@@ -325,7 +324,7 @@ class FutureS2MExtractor(S2MExtractor):
                 else:
                     list_geometry = self.conf.list_geometry
 
-                tb02.extend(toolbox.input(
+                tb02.extend(vortex.input(
                     alternate      = 'pro',
                     vapp           = 's2m',
                     vconf          = '[geometry::area]',
@@ -350,10 +349,10 @@ class FutureS2MExtractor(S2MExtractor):
         return self.get_std(tb02)
 
     def get_hydro(self):
-        import cen
+        import vortex_cen
 
         if self.conf.previ:
-            tb03 = toolbox.input(
+            tb03 = vortex.input(
                 role           = 'hydro',
                 vapp           = 's2m',
                 vconf          = '[geometry::area]',
@@ -373,7 +372,7 @@ class FutureS2MExtractor(S2MExtractor):
                 fatal          = False,
             )
         else:
-            tb03 = toolbox.input(
+            tb03 = vortex.input(
                 role = 'hydro',
                 vapp = 's2m',
                 vconf = '[geometry::area]',
@@ -398,9 +397,9 @@ class FutureS2MExtractor(S2MExtractor):
 
 
     def get_pp_quantiles(self):
-        import cen
+        import vortex_cen
 
-        tb_pp = toolbox.input(
+        tb_pp = vortex.input(
             role        = 'Postproc_output',
             intent      = 'in',
             vapp        = 's2m',
@@ -411,7 +410,8 @@ class FutureS2MExtractor(S2MExtractor):
             geometry    = self.conf.list_geometry,
             date        = self.conf.rundate,
             datebegin   = self.datebegin if self.conf.previ else '[dateend]/-PT24H',
-            dateend     = self.dateend if self.conf.previ else list(daterange(tomorrow(base=self.datebegin), self.dateend)),
+            dateend     = self.dateend if self.conf.previ else list(daterange(tomorrow(base=self.datebegin),
+                                                                              self.dateend)),
             nativefmt   = 'netcdf',
             kind        = 'SnowpackSimulation',
             model       = 'postproc',
@@ -464,7 +464,7 @@ if __name__ == "__main__":
                         help="Extract, potentially emos postprocessed, quantiles (fresh snow)")
     OPTIONS = PARSER.parse_args()
 
-    config_from_command = configcommanddev(OPTIONS) if OPTIONS.dev else configcommand(OPTIONS)
+    config_from_command = Configcommanddev(OPTIONS) if OPTIONS.dev else Configcommand(OPTIONS)
 
     S2ME = FutureS2MExtractor(conf=config_from_command)
 

commit c102163c1c29a8a02f51419fa7fbe4ed7980f8fd

Bugfix : Proplotter GUI bug introduced by 41e96bd

snowtools/plots/stratiprofile/proplotter_gui.py
@@ -631,7 +631,7 @@ class ProPlotterControllerEscroc(ProPlotterControllerSlider):
         Update the date scaler after opening a file.
         """
         if self.master.fileobj is not None:
-            self.master.choices.params_w.scale_date.Config(
+            self.master.choices.params_w.scale_date.config(
                 from_=0, to=(len(self.master.fileobj.get_time()) - 1),
                 state='normal', showvalue=0, variable=tk.IntVar,
                 command=lambda _: (self.master.choices.params_w.update_slice_date(), self.update_slice_date()))
@@ -715,7 +715,7 @@ class ProPlotterControllerMultiple(ProPlotterControllerSlider):
         Update the date scaler after opening a file.
         """
         if self.master.fileobj is not None:
-            self.master.choices.params_w.scale_date.Config(
+            self.master.choices.params_w.scale_date.config(
                 from_=0, to=(len(self.master.fileobj.get_time()) - 1),
                 state='normal', showvalue=0, variable=tk.IntVar,
                 command=lambda _: (self.master.choices.params_w.update_slice_date(), self.update_slice_date()))